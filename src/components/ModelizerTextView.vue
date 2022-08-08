adff83193fa82bad77031d7c248fd802f6c56359
Vincent Moittié <vincent.moittie@cji.paris>
2022-08-08 16:15:06 +0200

    Add monaco editor to ModelizerTextView

--- src/components/ModelizerTextView.vue
+++ src/components/ModelizerTextView.vue
@@ -2,9 +2,23 @@
   <div class="modelizer-text-view"
     data-cy="modelizer-text-view"
   >
-    ModelizerTextView
+    <monaco-editor :viewType="viewType"/>
   </div>
 </template>
 
 <script setup>
+import MonacoEditor from 'components/editor/MonacoEditor.vue';
+
+defineProps({
+  viewType: String,
+});
 </script>
+
+<style lang="scss" scoped>
+.modelizer-text-view {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  height: 100vh;
+}
+</style>
