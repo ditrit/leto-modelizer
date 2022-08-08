0d478700ef89b3c45035323e09c98a5e68603109
jRimbault <jacques.rimbault@gmail.com>
2022-08-08 15:39:04 +0200

    Add the components definitions panel to the modelizer page

--- src/components/ModelizerModelView.vue
+++ src/components/ModelizerModelView.vue
@@ -2,9 +2,11 @@
   <div class="modelizer-model-view"
     data-cy="modelizer-model-view"
   >
+    <component-definitions-panel/>
     ModelizerModelView
   </div>
 </template>
 
 <script setup>
+import ComponentDefinitionsPanel from 'src/components/drawer/ComponentDefinitionsPanel';
 </script>
