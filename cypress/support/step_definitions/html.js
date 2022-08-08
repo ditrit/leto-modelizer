b7fd3bd7e829ec0e5e3eb7b846464ecd51a3e89c
jRimbault <jacques.rimbault@gmail.com>
2022-08-08 15:39:18 +0200

    Add E2E test for the drawer

--- cypress/support/step_definitions/html.js
+++ cypress/support/step_definitions/html.js
@@ -1,4 +1,4 @@
-import { Then } from '@badeball/cypress-cucumber-preprocessor';
+import { Given, Then } from '@badeball/cypress-cucumber-preprocessor';
 import nunjucks from 'nunjucks';
 
 Then('I expect {string} exists', (templateSelector) => {
@@ -10,4 +10,8 @@ Then('I expect {string} is {string}', (templateSelector, templateExpectedValue) 
   const selector = nunjucks.renderString(templateSelector, cy.context);
   const expectedValue = nunjucks.renderString(templateExpectedValue, cy.context);
   cy.get(selector).contains(expectedValue);
+});
+
+Given('I set viewport size to {string} px for width and {string} px for height', (width, height) => {
+  cy.viewport(parseInt(width, 10), parseInt(height, 10));
 });
