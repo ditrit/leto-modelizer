249869fdf3cd1d36ce652edea9ab8138e73fa64c
k-y0u <pita8892@gmail.com>
2022-08-25 11:52:51 +0200

    Disable browser window automatic openning.

--- quasar.config.js
+++ quasar.config.js
@@ -89,7 +89,7 @@ module.exports = configure((ctx) => ({
       type: 'http',
     },
     port: 8080,
-    open: true, // opens browser window automatically
+    open: false, // opens browser window automatically
   },
 
   // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-framework
