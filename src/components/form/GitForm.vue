f057dfb0205ce21df645adada86128854b9e99ae
Vincent Moittié <vincent.moittie@cji.paris>
2022-09-21 13:33:30 +0200

    Set remote of git project on git form submit

--- src/components/form/GitForm.vue
+++ src/components/form/GitForm.vue
@@ -1,51 +1,58 @@
 <template>
   <q-form
-      @submit="onSubmit"
-      data-cy="git-form"
-      class="q-gutter-md git-form">
+    @submit="onSubmit"
+    data-cy="git-form"
+    class="q-gutter-md git-form">
     <q-input
-        filled
-        v-model="repository"
-        :label="$t('page.modelizer.settings.gitProvider.repository')"
-        lazy-rules
-        data-cy="git-repository-input"
-        :hint="$t('page.modelizer.settings.gitProvider.repositoryExample')"
-        :rules="[v => notEmpty($t, v), v => isGitRepositoryUrl($t, v)]"
+      filled
+      v-model="repository"
+      :label="$t('page.modelizer.settings.gitProvider.repository')"
+      lazy-rules
+      data-cy="git-repository-input"
+      :hint="$t('page.modelizer.settings.gitProvider.repositoryExample')"
+      :rules="[v => notEmpty($t, v), v => isGitRepositoryUrl($t, v)]"
     />
 
     <q-input
-        filled
-        v-model="username"
-        :label="$t('page.modelizer.settings.gitProvider.username')"
-        lazy-rules
-        data-cy="git-username-input"
-        :rules="[v => notEmpty($t, v)]"
+      filled
+      v-model="username"
+      :label="$t('page.modelizer.settings.gitProvider.username')"
+      lazy-rules
+      data-cy="git-username-input"
+      :rules="[v => notEmpty($t, v)]"
     />
 
     <q-input
-        filled
-        v-model="token"
-        :label="$t('page.modelizer.settings.gitProvider.token')"
-        lazy-rules
-        data-cy="git-token-input"
-        :rules="[v => notEmpty($t, v)]"
+      filled
+      v-model="token"
+      :label="$t('page.modelizer.settings.gitProvider.token')"
+      lazy-rules
+      data-cy="git-token-input"
+      :rules="[v => notEmpty($t, v)]"
     />
 
     <div class="flex row items-center justify-center">
       <q-btn
-          icon="fa-solid fa-save"
-          :label="$t('actions.default.save')"
-          type="submit"
-          data-cy="git-form-submit"
-          color="positive"/>
+        icon="fa-solid fa-save"
+        :label="$t('actions.default.save')"
+        type="submit"
+        :loading="submitting"
+        data-cy="git-form-submit"
+        color="positive">
+        <template v-slot:loading>
+          <q-spinner-dots/>
+        </template>
+      </q-btn>
     </div>
   </q-form>
 </template>
 
 <script setup>
+import { Notify } from 'quasar';
+import { useI18n } from 'vue-i18n';
 import { ref } from 'vue';
 import { notEmpty, isGitRepositoryUrl } from 'src/composables/QuasarFieldRule';
-import { getProjectById, saveProject } from 'src/composables/Project';
+import { getProjectById, saveProject, updateGitProject } from 'src/composables/Project';
 
 const emit = defineEmits(['project-git:save']);
 
@@ -53,19 +60,40 @@ const props = defineProps({
   projectName: String,
 });
 
+const { t } = useI18n();
 const project = getProjectById(props.projectName);
 const repository = ref(project.git?.repository);
 const username = ref(project.git?.username);
 const token = ref(project.git?.token);
+const submitting = ref(false);
 
 function onSubmit() {
+  submitting.value = true;
   project.git = {
     repository: repository.value,
     username: username.value,
     token: token.value,
   };
-  saveProject(project);
-  emit('project-git:save');
+  return updateGitProject(project)
+    .then(() => {
+      saveProject(project);
+      emit('project-git:save');
+      Notify.create({
+        type: 'positive',
+        message: t('actions.git.repository.exists'),
+        html: true,
+      });
+    })
+    .catch(({ name }) => {
+      Notify.create({
+        type: 'warning',
+        message: t(`errors.git.${name}`),
+        html: true,
+      });
+    })
+    .finally(() => {
+      submitting.value = false;
+    });
 }
 
 </script>
--- tests/unit/components/form/GitForm.spec.js
+++ tests/unit/components/form/GitForm.spec.js
@@ -1,17 +1,50 @@
 import { installQuasarPlugin } from '@quasar/quasar-app-extension-testing-unit-jest';
 import { shallowMount } from '@vue/test-utils';
 import GitForm from 'src/components/form/GitForm.vue';
-import { getProjectById, saveProject } from 'src/composables/Project';
+import {
+  getProjectById,
+  saveProject,
+  updateGitProject,
+} from 'src/composables/Project';
+import { Notify } from 'quasar';
 
-installQuasarPlugin();
+installQuasarPlugin({
+  plugins: [Notify],
+});
+
+jest.mock('vue-i18n', () => ({
+  useI18n: () => ({
+    t: (t) => t,
+  }),
+}));
+
+jest.mock('src/composables/Project', () => ({
+  getProjectById: jest.fn((id) => {
+    if (id === 'noGit') {
+      return { id };
+    }
+    return {
+      id,
+      git: {
+        repository: 'testRepository',
+        username: 'testUsername',
+        token: 'testToken',
+      },
+    };
+  }),
+  saveProject: jest.fn(),
+  updateGitProject: jest.fn((project) => {
+    if (project.id === 'error') {
+      return Promise.reject({ name: 'HttpError' });
+    }
+    return Promise.resolve();
+  }),
+}));
 
 describe('Test component: GitForm', () => {
   let wrapper;
 
   beforeEach(() => {
-    saveProject({
-      id: 'test',
-    });
     wrapper = shallowMount(GitForm, {
       props: {
         projectName: 'test',
@@ -27,67 +60,87 @@ describe('Test component: GitForm', () => {
     });
 
     describe('Test variable: repository', () => {
-      it('should be undefined on no git configuration in project', () => {
+      it('should be undefined without any git configuration in project', () => {
+        wrapper = shallowMount(GitForm, {
+          props: {
+            projectName: 'noGit',
+          },
+        });
         expect(wrapper.vm.repository).toBeUndefined();
       });
 
-      it('should be not null on git configuration in project', () => {
-        saveProject({ id: 'test', git: { repository: 'testRepository' } });
-        wrapper = shallowMount(GitForm, {
-          props: {
-            projectName: 'test',
-          },
-        });
+      it('should not be null with git configuration in project', () => {
         expect(wrapper.vm.repository).toEqual('testRepository');
       });
     });
 
     describe('Test variable: token', () => {
-      it('should be undefined on no git configuration in project', () => {
+      it('should be undefined without any git configuration in project', () => {
+        wrapper = shallowMount(GitForm, {
+          props: {
+            projectName: 'noGit',
+          },
+        });
         expect(wrapper.vm.token).toBeUndefined();
       });
 
-      it('should be not null on git configuration in project', () => {
-        saveProject({ id: 'test', git: { token: 'testToken' } });
-        wrapper = shallowMount(GitForm, {
-          props: {
-            projectName: 'test',
-          },
-        });
+      it('should not be null with git configuration in project', () => {
         expect(wrapper.vm.token).toEqual('testToken');
       });
     });
 
     describe('Test variable: username', () => {
-      it('should be undefined on no git configuration in project', () => {
+      it('should be undefined without any git configuration in project', () => {
+        wrapper = shallowMount(GitForm, {
+          props: {
+            projectName: 'noGit',
+          },
+        });
         expect(wrapper.vm.username).toBeUndefined();
       });
 
-      it('should be not null on git configuration in project', () => {
-        saveProject({ id: 'test', git: { username: 'TestUsername' } });
-        wrapper = shallowMount(GitForm, {
-          props: {
-            projectName: 'test',
-          },
-        });
-        expect(wrapper.vm.username).toEqual('TestUsername');
+      it('should not be null with git configuration in project', () => {
+        expect(wrapper.vm.username).toEqual('testUsername');
       });
     });
   });
 
   describe('Test function: onSubmit', () => {
-    it('should emit an event', () => {
+    it('should emit an event on success', async () => {
       wrapper.vm.repository = 'https://test/test.git';
       wrapper.vm.token = 'testToken';
       wrapper.vm.username = 'TestUsername';
-      wrapper.vm.onSubmit();
 
-      expect(getProjectById('test').git).toEqual({
-        repository: 'https://test/test.git',
-        token: 'testToken',
-        username: 'TestUsername',
-      });
+      Notify.create = jest.fn();
+
+      await wrapper.vm.onSubmit();
+
+      expect(getProjectById).toBeCalled();
+      expect(saveProject).toBeCalled();
+      expect(updateGitProject).toBeCalled();
+      expect(wrapper.vm.submitting).toEqual(false);
       expect(wrapper.emitted()['project-git:save']).toBeTruthy();
+      expect(Notify.create).toHaveBeenCalledWith(expect.objectContaining({ type: 'positive' }));
+    });
+
+    it('should emit a notification on error', async () => {
+      wrapper = shallowMount(GitForm, {
+        props: {
+          projectName: 'error',
+        },
+      });
+
+      Notify.create = jest.fn();
+
+      wrapper.vm.repository = 'https://test/test.git';
+      wrapper.vm.token = 'testToken';
+      wrapper.vm.username = 'TestUsername';
+      await wrapper.vm.onSubmit();
+
+      expect(getProjectById).toBeCalled();
+      expect(saveProject).toBeCalled();
+      expect(updateGitProject).toBeCalled();
+      expect(Notify.create).toHaveBeenCalledWith(expect.objectContaining({ type: 'warning' }));
     });
   });
 });
