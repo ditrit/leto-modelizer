2845fabfd67c79c25d4a2a98d386776f23982670
Justine <hell.justine@gmail.com>
2022-09-23 15:21:39 +0200

    add FileExplorer into ModelizerTextView display tree with hard-coded data

--- src/components/ModelizerTextView.vue
+++ src/components/ModelizerTextView.vue
@@ -1,24 +1,38 @@
 <template>
-  <div class="modelizer-text-view"
+  <div class="modelizer-text-view row"
     data-cy="modelizer-text-view"
   >
-    <monaco-editor :viewType="viewType"/>
+    <file-explorer
+      class="q-ma-md col-2 overflow-auto"
+      :nodes="nodes"
+    />
+    <q-separator vertical inset />
+    <monaco-editor :viewType="viewType" class="col-auto" />
   </div>
 </template>
 
 <script setup>
+import { getTree } from 'src/composables/FileExplorer';
 import MonacoEditor from 'components/editor/MonacoEditor.vue';
+import FileExplorer from 'components/FileExplorer.vue';
 
 defineProps({
   viewType: String,
 });
-</script>
 
-<style lang="scss" scoped>
-.modelizer-text-view {
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  height: 100vh;
-}
-</style>
+// TODO : Remove Hard-coded data when link with git provider will be done
+const fileInformation = [
+  { path: '.gitignore' },
+  { path: '/home/terraform/app.tf' },
+  { path: '/home/terraform/user.tf' },
+  { path: '/tests/example.spec.js' },
+  { path: '/tests/unit/file3.tf' },
+  { path: '/tests/unit/events.spec.js' },
+  { path: 'README.md' },
+  { path: '/home/file1.tf' },
+  { path: '/home/file2.tf' },
+];
+
+const nodes = getTree(fileInformation);
+
+</script>
