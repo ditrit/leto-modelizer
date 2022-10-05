32ec67dfa8f02b6b87b676183a0af1ee4f4d0ccb
Vincent Moittié <vincent.moittie@cji.paris>
2022-10-05 14:26:52 +0200

    Add fetch event on GitEvent

--- tests/unit/composables/events/GitEvent.spec.js
+++ tests/unit/composables/events/GitEvent.spec.js
@@ -1,11 +1,18 @@
-import UpdateRemoteEvent from 'src/composables/events/GitEvent';
+import GitEvent from 'src/composables/events/GitEvent';
 import { Subject } from 'rxjs';
 
 describe('Test composable: GitEvent', () => {
   describe('Test event: UpdateRemoteEvent', () => {
     it('Should export a Subject', () => {
-      expect(UpdateRemoteEvent).toBeDefined();
-      expect(UpdateRemoteEvent).toEqual(new Subject());
+      expect(GitEvent.UpdateRemoteEvent).toBeDefined();
+      expect(GitEvent.UpdateRemoteEvent).toEqual(new Subject());
+    });
+  });
+
+  describe('Test event: FetchEvent', () => {
+    it('Should export a Subject', () => {
+      expect(GitEvent.FetchEvent).toBeDefined();
+      expect(GitEvent.FetchEvent).toEqual(new Subject());
     });
   });
 });
