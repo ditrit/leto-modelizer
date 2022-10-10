ef2dd2c4679f0c1deecc4fec72ba6d7628b47d1a
Vincent Moittié <vincent.moittie@cji.paris>
2022-10-10 15:28:12 +0200

    Add search input in branch menu

--- src/components/menu/GitBranchMenu.vue
+++ src/components/menu/GitBranchMenu.vue
@@ -1,43 +1,70 @@
 <template>
-  <q-menu data-cy="git-branch-menu">
+  <q-menu
+    class="git-branch-menu"
+    data-cy="git-branch-menu"
+    @show="onOpenMenu"
+  >
     <q-list style="min-width: 500px">
-      <git-branch-header-menu :title="$t('menu.git.localBranchesTitle')"/>
-      <template v-for="(branch, index) in branches.local">
-        <git-branch-item-menu
-          :key="`local_${branch.name}`"
-          :name="branch.name"
-          :full-name="branch.fullName"
-          :current="branch.name === currentBranchName"
-          v-if="showLocal || index < maxItem"
-          :data-cy="`git-menu-branch-local-${branch.name}`"
+      <q-input
+        ref="searchInput"
+        data-cy="git-branch-menu-search-bar"
+        v-model="searchedBranch"
+        dense
+        clearable
+        @update:model-value="filter"
+      >
+        <template v-slot:prepend>
+          <q-icon name="fa-solid fa-magnifying-glass" />
+        </template>
+      </q-input>
+
+      <template v-if="filteredBranches.local.length > 0">
+        <git-branch-header-menu :title="$t('menu.git.localBranchesTitle')"/>
+        <template v-for="(branch, index) in filteredBranches.local">
+          <git-branch-item-menu
+            :key="`local_${branch.name}`"
+            :name="branch.name"
+            :full-name="branch.fullName"
+            :current="branch.name === currentBranchName"
+            v-if="showLocal || index < maxItem"
+            :data-cy="`git-menu-branch-local-${branch.name}`"
+          />
+        </template>
+        <git-branch-expand-list-menu
+          data-cy="git-branch-expand-local-menu"
+          :open="showLocal"
+          :number="filteredBranches.local.length - maxItem"
+          v-if="filteredBranches.local.length > maxItem"
+          @click="openCloseExpandMenu(true)"
         />
       </template>
-      <git-branch-expand-list-menu
-        :open="showLocal"
-        :number="branches.local.length - maxItem"
-        v-if="branches.local.length > maxItem"
-        @click="openCloseExpandMenu(true)"
-      />
 
-      <q-separator/>
-
-      <git-branch-header-menu :title="$t('menu.git.remoteBranchesTitle')"/>
-      <template v-for="(branch, index) in branches.remote">
-        <git-branch-item-menu
-          :key="`remote_${branch.name}`"
-          :name="branch.name"
-          :full-name="branch.fullName"
-          :current="branch.name === currentBranchName"
-          v-if="showRemote || index < maxItem"
-          :data-cy="`git-menu-branch-remote-${branch.name}`"
+      <template v-if="filteredBranches.remote.length > 0">
+        <git-branch-header-menu :title="$t('menu.git.remoteBranchesTitle')"/>
+        <template v-for="(branch, index) in filteredBranches.remote">
+          <git-branch-item-menu
+            :key="`remote_${branch.name}`"
+            :name="branch.name"
+            :full-name="branch.fullName"
+            :current="branch.name === currentBranchName"
+            v-if="showRemote || index < maxItem"
+            :data-cy="`git-menu-branch-remote-${branch.name}`"
+          />
+        </template>
+        <git-branch-expand-list-menu
+          data-cy="git-branch-expand-remote-menu"
+          :open="showRemote"
+          :number="filteredBranches.remote.length - maxItem"
+          v-if="filteredBranches.remote.length > maxItem"
+          @click="openCloseExpandMenu(false)"
         />
       </template>
-      <git-branch-expand-list-menu
-        :open="showRemote"
-        :number="branches.remote.length - maxItem"
-        v-if="branches.remote.length > maxItem"
-        @click="openCloseExpandMenu(false)"
-      />
+
+      <template v-if="hasNoBranches">
+        <q-item>
+          <q-item-section>{{ $t('menu.git.noBranches')}}</q-item-section>
+        </q-item>
+      </template>
     </q-list>
   </q-menu>
 </template>
@@ -45,13 +72,18 @@
 <script setup>
 import GitBranchItemMenu from 'components/menu/GitBranchItemMenu';
 import GitBranchHeaderMenu from 'components/menu/GitBranchHeaderMenu';
-import { onMounted, onUnmounted, ref } from 'vue';
+import {
+  computed,
+  onMounted,
+  onUnmounted,
+  ref,
+} from 'vue';
 import { useRoute } from 'vue-router';
 import { getBranches, fetchGit, getProjectById } from 'src/composables/Project';
 import GitEvent from 'src/composables/events/GitEvent';
 import GitBranchExpandListMenu from 'components/menu/GitBranchExpandListMenu';
 
-defineProps({
+const props = defineProps({
   currentBranchName: {
     type: String,
     required: true,
@@ -60,13 +92,58 @@ defineProps({
 
 const route = useRoute();
 const maxItem = ref(5);
+const filteredBranches = ref({
+  local: [],
+  remote: [],
+});
 const branches = ref({
   local: [],
   remote: [],
 });
+const searchedBranch = ref('');
 const showLocal = ref(false);
 const showRemote = ref(false);
+const searchInput = ref(null);
+const hasNoBranches = computed(() => filteredBranches.value.local.length === 0
+    && filteredBranches.value.remote.length === 0);
 let fetchSubscription;
+
+/**
+ * On open menu, focus on the search input and close expand menu.
+ */
+function onOpenMenu() {
+  searchInput.value.focus();
+  showLocal.value = false;
+  showRemote.value = false;
+}
+
+/**
+ * Indicate if branch name contains searched text.
+ * @param {String} branch - Branch name.
+ * @return {Boolean} Return true if branch contains searched text otherwise return false.
+ */
+function isSearched(branch) {
+  if (!searchedBranch.value || searchedBranch.value.trim().length === 0) {
+    return true;
+  }
+
+  return searchedBranch.value.toLowerCase().split(' ')
+    .filter((searchedText) => searchedText !== '')
+    .some((searchedText) => branch.toLowerCase().includes(searchedText));
+}
+
+/**
+ * Filter all branches with searched text.
+ */
+function filter() {
+  filteredBranches.value.local = branches.value.local
+    .filter((branch) => isSearched(branch.name) || isSearched(branch.fullName))
+    .sort((a, b) => a.compare(b));
+
+  filteredBranches.value.remote = branches.value.remote
+    .filter((branch) => isSearched(branch.name) || isSearched(branch.fullName))
+    .sort((a, b) => a.compare(b));
+}
 
 /**
  * Change state value of expand menu.
@@ -85,14 +162,20 @@ function openCloseExpandMenu(local) {
  */
 async function initBranches() {
   const allBranches = await getBranches(route.params.projectName, true);
+  const local = [];
+  const remote = [];
   allBranches.forEach((branch) => {
+    branch.current = props.currentBranchName === branch.name;
     if (branch.onLocal) {
-      branches.value.local.push(branch);
+      local.push(branch);
     }
     if (branch.onRemote) {
-      branches.value.remote.push(branch);
+      remote.push(branch);
     }
   });
+  branches.value.local = local;
+  branches.value.remote = remote;
+  filter();
 }
 
 onMounted(() => {
