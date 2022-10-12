e945b7a7f801110785d04f9233e54111dcd3b40d
Vincent Moittié <moittie.vincent@gmail.com>
2022-10-12 15:40:23 +0200

    Add form to create branch

---
+++ src/components/form/GitNewBranchForm.vue
@@ -0,0 +1,96 @@
+<template>
+  <q-form
+    ref="form"
+    @submit="onSubmit"
+    data-cy="git-newBranch-form"
+    class="q-gutter-md git-newBranch-form"
+  >
+    <q-input
+      filled
+      v-model="newBranch"
+      :label="$t('page.modelizer.git.newBranch.branch')"
+      lazy-rules
+      data-cy="git-branch-input"
+      :rules="[(v) => notEmpty(t, v), (v) => isUniqueBranchName(t, branches, v)]"
+    />
+    <q-checkbox
+      v-model="checkout"
+      data-cy="git-checkout-checkbox"
+      :label="$t('page.modelizer.git.newBranch.checkout')"
+    />
+    <div class="flex row items-center justify-center">
+      <q-btn
+        icon="fa-solid fa-save"
+        :label="$t('actions.default.create')"
+        type="submit"
+        :loading="submitting"
+        data-cy="git-form-submit"
+        color="positive"
+      >
+        <template v-slot:loading>
+          <q-spinner-dots/>
+        </template>
+      </q-btn>
+    </div>
+  </q-form>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import { notEmpty, isUniqueBranchName } from 'src/composables/QuasarFieldRule';
+import { getBranches, createBranchFrom } from 'src/composables/Project';
+import { useI18n } from 'vue-i18n';
+import { Notify } from 'quasar';
+
+const emit = defineEmits(['git-branch:create']);
+
+const props = defineProps({
+  projectName: {
+    type: String,
+    required: true,
+  },
+  branchName: {
+    type: String,
+    required: true,
+  },
+});
+
+const { t } = useI18n();
+const form = ref(null);
+const newBranch = ref(null);
+const checkout = ref(true);
+const submitting = ref(false);
+const branches = ref([]);
+
+/**
+ * Create new branch and manage toast and loader.
+ */
+function onSubmit() {
+  submitting.value = true;
+  return createBranchFrom(props.projectName, newBranch.value, props.branchName, checkout.value)
+    .then(() => {
+      emit('git-branch:create');
+      Notify.create({
+        type: 'positive',
+        message: t('actions.git.branch.create'),
+        html: true,
+      });
+    })
+    .catch(({ name }) => {
+      Notify.create({
+        type: 'negative',
+        message: t(`errors.git.${name}`),
+        html: true,
+      });
+    })
+    .finally(() => {
+      submitting.value = false;
+    });
+}
+
+onMounted(() => {
+  getBranches(props.projectName).then((array) => {
+    branches.value = array;
+  });
+});
+</script>
