dda5ff169447fff53398253f0e37f0f95efe4b9e
Justine Hell <hell.justine@gmail.com>
2022-10-13 15:28:05 +0200

    update data for SelectFileEvent

--- src/composables/events/FileEvent.js
+++ src/composables/events/FileEvent.js
@@ -4,15 +4,15 @@ import { Subject } from 'rxjs';
  * Represent a rxjs Event object to emit and to receive events about file opening.
  * The event should be an Object that contains the `id` (path), the `label`
  * and the `content` of the file.
- * Example: { id: 'terraform/app.tf', label: 'app.tf', content: 'Hello World' },
+ * Example: { id: 'terraform/app.tf', label: 'app.tf', content: 'Hello World' }
  * @typedef {Subject} OpenFileEvent
  */
 const OpenFileEvent = new Subject();
 
 /**
  * Represent a rxjs Event object to emit and to receive events about file selecting.
- * The event should be the selected file's id.
- * Example: 'terraform/app.tf' or '' when no file is selected.
+ * The event should be an Object that contains the `id` (path) of the file and `isSelected` boolean.
+ * Example: { isSelected: true, id: 'terraform/app.tf' }
  * @typedef {Subject} SelectFileEvent
  */
 const SelectFileEvent = new Subject();
