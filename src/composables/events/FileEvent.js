5e19218f05e8d8bd14038f3f45e579ea553bf266
Justine Hell <hell.justine@gmail.com>
2022-10-24 17:38:33 +0200

    Add events

--- src/composables/events/FileEvent.js
+++ src/composables/events/FileEvent.js
@@ -4,20 +4,52 @@ import { Subject } from 'rxjs';
  * Represent a rxjs Event object to emit and to receive events about file opening.
  * The event should be an Object that contains the `id` (path), the `label`
  * and the `content` of the file.
- * Example: { id: 'terraform/app.tf', label: 'app.tf', content: 'Hello World' }
  * @typedef {Subject} OpenFileEvent
  */
 const OpenFileEvent = new Subject();
 
 /**
  * Represent a rxjs Event object to emit and to receive events about file selecting.
- * The event should be an Object that contains the `id` (path) of the file and `isSelected` boolean.
+ * The event should be an Object that contains the `id` (path) of the file and `isSelected`.
  * Example: { isSelected: true, id: 'terraform/app.tf' }
  * @typedef {Subject} SelectFileEvent
  */
 const SelectFileEvent = new Subject();
 
+/**
+ * Represent a rxjs Event object to emit and to receive events about node selection.
+ * The event should be an Object that contains the `id` (path), `label`, `icon` and `isFolder`.
+ * The Object should also contain `isNewLocalFile` for a file, or `children` for a folder.
+ * @typedef {Subject} SelectFileEvent
+ */
+const SelectNodeEvent = new Subject();
+
+/**
+ * Represent a rxjs Event object to emit and to receive events about file creation.
+ * The event should be an Object that contains the `name` (path) of the file and `isFolder`.
+ * @typedef {Subject} SelectFileEvent
+ */
+const CreateFileEvent = new Subject();
+
+/**
+ * Represent a rxjs Event object to emit and to receive events about node deletion.
+ * The event should be an Object that contains `deletedNode` for the node Object to delete
+ * and `parentNode` for the deleted node's parent Object.
+ * @typedef {Subject} SelectFileEvent
+ */
+const DeleteFileEvent = new Subject();
+
+/**
+ * Represent a rxjs Event object to emit and to receive events about node expansion.
+ * @typedef {Subject} SelectFileEvent
+ */
+const ExpandFolderEvent = new Subject();
+
 export default {
   OpenFileEvent,
   SelectFileEvent,
+  SelectNodeEvent,
+  CreateFileEvent,
+  DeleteFileEvent,
+  ExpandFolderEvent,
 };
