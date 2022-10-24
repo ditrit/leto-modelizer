7f59e73eb2c413c3160a4e272f0d447f088853d8
Justine Hell <hell.justine@gmail.com>
2022-10-24 17:39:47 +0200

    Add quasar rules

--- src/composables/QuasarFieldRule.js
+++ src/composables/QuasarFieldRule.js
@@ -20,6 +20,7 @@ export function isGitRepositoryUrl(t, value) {
   return /^http(s)?:\/\/.+\/.*(?<!\.git)$/.test(value)
     || t('errors.invalid.gitProvider.repository');
 }
+
 /**
  * Check if the branch name does not already exist.
  * @param {Function} t - I18n translate function.
@@ -31,4 +32,29 @@ export function isGitRepositoryUrl(t, value) {
 export function isUniqueBranchName(t, branches, value) {
   return branches.every((branch) => branch.name !== value)
     || t('errors.git.branch.duplicate');
+}
+
+/**
+ * Check if value is a valid file label.
+ * @param {Function} t - I18n translate function.
+ * @param {String} value - Value to check.
+ * @return {boolean|String} Return true if valid otherwise the translated error message.
+ */
+export function isValidFileLabel(t, value) {
+  return !value.includes('/')
+    || t('errors.invalid.fileExplorer.label');
+}
+
+/**
+ * Check if a file's label does not already exist.
+ * @param {Function} t - I18n translate function.
+ * @param {Object[]} tree - Tree Object.
+ * @param {String} tree.label - Label of a file.
+ * @param {String} value - Value to check.
+ * @return {boolean|String} Return true if the node label doesn't already exist,
+ * otherwise the translated error message.
+ */
+export function isUniqueFileLabel(t, tree, value) {
+  return tree.every(({ label }) => label !== value)
+    || t('errors.fileExplorer.label.duplicate');
 }
