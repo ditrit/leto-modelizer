c16629794c522d24ff7bdf54998e94924fe6a4fd
Justine Hell <hell.justine@gmail.com>
2022-10-24 18:04:35 +0200

    Add form and dialog to create file

---
+++ src/components/dialog/CreateFileDialog.vue
@@ -0,0 +1,58 @@
+<template>
+  <default-dialog dialog-key="CreateFile" data-cy="create-file-dialog">
+    <template v-slot:title>
+      <q-icon
+        color="primary"
+        name="fa-solid fa-file-circle-plus"
+      />
+      {{ $t(`page.modelizer.fileExplorer.create.${onFolder ? 'folder' : 'file'}.title`) }}
+    </template>
+    <template v-slot:default>
+      <create-file-form
+        :project-name="projectName"
+        :file="newFile"
+        :isFolder="onFolder"
+        @file:create="DialogEvent.next({ type: 'close', key: 'CreateFile' })"
+      />
+    </template>
+  </default-dialog>
+</template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+import DialogEvent from 'src/composables/events/DialogEvent';
+import DefaultDialog from 'components/dialog/DefaultDialog';
+import CreateFileForm from 'components/form/CreateFileForm';
+
+defineProps({
+  projectName: {
+    type: String,
+    required: true,
+  },
+});
+
+const newFile = ref(null);
+const onFolder = ref(null);
+let dialogEventSubscription;
+
+/**
+ * Set newFile and onFolder on valid event.
+ * @param {String} key - Event key.
+ * @param {Object} file - Event file.
+ * @param {Boolean} isFolder - True if file node is a folder, otherwise false.
+ */
+function setFileNode({ key, file, isFolder }) {
+  if (key === 'CreateFile') {
+    newFile.value = file;
+    onFolder.value = isFolder;
+  }
+}
+
+onMounted(() => {
+  dialogEventSubscription = DialogEvent.subscribe(setFileNode);
+});
+
+onUnmounted(() => {
+  dialogEventSubscription.unsubscribe();
+});
+</script>
