cdca3b2cf286d579adcb498a621d48b37dc50002
ptanghe <pita8892@gmail.com>
2022-11-02 17:45:30 +0100

    Add InputWrapper

---
+++ src/components/inputs/InputWrapper.vue
@@ -0,0 +1,26 @@
+<template>
+  <component
+    :is="inputList[attribute.type]"
+    :attribute="attribute"
+    @update:model-value="(event) => emit('update:attribute', event)"
+  />
+</template>
+
+<script setup>
+import { defineAsyncComponent } from 'vue';
+
+defineProps({
+  attribute: {
+    type: Object,
+    required: true,
+  },
+});
+
+defineEmits(['update:attribute']);
+
+const inputList = {
+  Boolean: defineAsyncComponent(() => import('./BooleanInput')),
+  String: defineAsyncComponent(() => import('./StringInput')),
+  Number: defineAsyncComponent(() => import('./NumberInput')),
+};
+</script>
