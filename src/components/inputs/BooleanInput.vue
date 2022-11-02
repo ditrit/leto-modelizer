673a987a885d9d146cc92aa12a2292803f7f24ca
ptanghe <pita8892@gmail.com>
2022-11-02 17:44:27 +0100

    Add BooleanInput, NumberInput & StringInput

---
+++ src/components/inputs/BooleanInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-toggle
+    v-model="localValue"
+    :label="attribute.name"
+    @update:model-value="(event) => emit('update:model-value', event)"
+  />
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+  attribute: {
+    type: Object,
+    required: true,
+  },
+});
+
+defineEmits(['update:model-value']);
+
+const localValue = ref(props.attribute.value);
+</script>
