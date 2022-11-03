04e4a7514a2bbe04e64dfab57152d9351102dc0c
Justine Hell <hell.justine@gmail.com>
2022-11-03 12:36:46 +0100

    Update FileExplorerActionMenu with git add action

--- src/components/menu/FileExplorerActionMenu.vue
+++ src/components/menu/FileExplorerActionMenu.vue
@@ -1,32 +1,32 @@
 <template>
   <q-menu
+    ref="menu"
     @hide="emit('hide:menu')"
-    class="tree-action-menu"
-    data-cy="tree-action-menu"
-    auto-close
+    class="file-explorer-menu"
+    data-cy="file-explorer-menu"
   >
     <q-list style="min-width: 150px">
       <template v-if="file.isFolder">
-        <q-item clickable @click="addFile(true)">
+        <q-item clickable @click="createFile(true)">
           <q-item-section avatar>
             <q-icon
               color="primary"
               size="xs"
               name="fa-solid fa-folder-plus"
-              data-cy="tree-action-add-folder"
+              data-cy="file-explorer-menu-create-folder"
             />
           </q-item-section>
           <q-item-section>
             {{ $t('actions.fileExplorer.newFolder') }}
           </q-item-section>
         </q-item>
-        <q-item clickable @click="addFile(false)">
+        <q-item clickable @click="createFile(false)">
           <q-item-section avatar>
             <q-icon
               color="primary"
               size="xs"
               name="fa-solid fa-file-circle-plus"
-              data-cy="tree-action-add-file"
+              data-cy="file-explorer-menu-create-file"
             />
           </q-item-section>
           <q-item-section>
@@ -34,13 +34,37 @@
           </q-item-section>
         </q-item>
       </template>
+      <template v-if="isFile && allowGitAdd">
+        <q-item
+          clickable
+          @click="addFile(file)"
+        >
+          <q-item-section avatar>
+            <q-icon
+              color="primary"
+              size="xs"
+              name="fa-solid fa-plus"
+              data-cy="file-explorer-menu-add-file"
+            />
+          </q-item-section>
+          <q-item-section>
+            {{ $t('actions.fileExplorer.addFile') }}
+          </q-item-section>
+        </q-item>
+        <q-linear-progress
+          v-if="loading.add"
+          data-cy="file-explorer-menu-add-loader"
+          color="primary"
+          indeterminate
+        />
+      </template>
       <q-item v-if="!file.isRootFolder" clickable @click="deleteFile">
         <q-item-section avatar>
           <q-icon
             color="primary"
             size="xs"
             name="fa-solid fa-trash"
-            data-cy="tree-action-delete-file"
+            data-cy="file-explorer-menu-delete-file"
           />
         </q-item-section>
         <q-item-section>
@@ -52,7 +76,12 @@
 </template>
 
 <script setup>
+import { computed, ref } from 'vue';
+import { gitAdd } from 'src/composables/Project';
+import { Notify } from 'quasar';
+import { useI18n } from 'vue-i18n';
 import DialogEvent from 'src/composables/events/DialogEvent';
+import FileEvent from 'src/composables/events/FileEvent';
 
 const emit = defineEmits(['hide:menu']);
 
@@ -61,18 +90,33 @@ const props = defineProps({
     type: Object,
     required: true,
   },
+  projectName: {
+    type: String,
+    required: true,
+  },
 });
+
+const { t } = useI18n();
+const menu = ref(null);
+const loading = ref({
+  add: false,
+});
+const isFile = computed(() => !props.file.isFolder);
+const allowGitAdd = computed(() => !!props.file.information?.isUntracked
+  || !!props.file.information?.isUnstaged
+  || !!props.file.information?.hasUnstagedChanged);
 
 /**
  * Send event to open the CreateFileDialog.
  */
-function addFile(isFolder) {
+function createFile(isFolder) {
   DialogEvent.next({
     type: 'open',
     key: 'CreateFile',
     file: props.file,
     isFolder,
   });
+  menu.value.hide();
 }
 
 /**
@@ -84,5 +128,33 @@ function deleteFile() {
     key: 'DeleteFile',
     file: props.file,
   });
+  menu.value.hide();
+}
+
+/**
+ * Add selected file and send UpdateFile event.
+ */
+function addFile(file) {
+  loading.value.add = true;
+  return gitAdd(props.projectName, file.id)
+    .then(() => {
+      Notify.create({
+        type: 'positive',
+        message: t('actions.fileExplorer.file.add'),
+        html: true,
+      });
+      FileEvent.UpdateFileEvent.next(file.id);
+    })
+    .catch(() => {
+      Notify.create({
+        type: 'negative',
+        message: t('errors.fileExplorer.default.file.add'),
+        html: true,
+      });
+    })
+    .finally(() => {
+      loading.value.add = false;
+      menu.value.hide();
+    });
 }
 </script>
--- tests/unit/components/menu/FileExplorerActionMenu.spec.js
+++ tests/unit/components/menu/FileExplorerActionMenu.spec.js
@@ -2,8 +2,26 @@ import { shallowMount } from '@vue/test-utils';
 import { installQuasarPlugin } from '@quasar/quasar-app-extension-testing-unit-jest';
 import DialogEvent from 'src/composables/events/DialogEvent';
 import FileExplorerActionMenu from 'components/menu/FileExplorerActionMenu';
+import { Notify } from 'quasar';
 
-installQuasarPlugin();
+installQuasarPlugin({
+  plugins: [Notify],
+});
+
+jest.mock('vue-i18n', () => ({
+  useI18n: () => ({
+    t: (t) => t,
+  }),
+}));
+
+jest.mock('src/composables/Project', () => ({
+  gitAdd: jest.fn((projectId) => {
+    if (projectId === 'error') {
+      return Promise.reject({ name: 'error' });
+    }
+    return Promise.resolve();
+  }),
+}));
 
 describe('Test component: FileExplorerActionMenu', () => {
   let wrapper;
@@ -12,7 +30,16 @@ describe('Test component: FileExplorerActionMenu', () => {
     wrapper = shallowMount(FileExplorerActionMenu, {
       props: {
         file: { id: 'test' },
+        projectName: 'projectName',
       },
+    });
+  });
+
+  describe('Test variables initialization', () => {
+    describe('Test variable: loading', () => {
+      it('should be false', () => {
+        expect(wrapper.vm.loading).toEqual({ add: false });
+      });
     });
   });
 
@@ -22,10 +49,120 @@ describe('Test component: FileExplorerActionMenu', () => {
         expect(wrapper.vm.file).toStrictEqual({ id: 'test' });
       });
     });
+
+    describe('Test prop: projectName', () => {
+      it('should match "projectName"', () => {
+        expect(wrapper.vm.projectName).toStrictEqual('projectName');
+      });
+    });
+  });
+
+  describe('Test computed', () => {
+    describe('Test computed: isFile', () => {
+      it('should be true if props.file is not a folder', async () => {
+        await wrapper.setProps({
+          file: {
+            isFolder: false,
+          },
+        });
+
+        expect(wrapper.vm.isFile).toEqual(true);
+      });
+
+      it('should be false if props.file is a folder', async () => {
+        await wrapper.setProps({
+          file: {
+            isFolder: true,
+          },
+        });
+
+        expect(wrapper.vm.isFile).toEqual(false);
+      });
+    });
+
+    describe('Test computed: allowGitAdd', () => {
+      it('should be true if props.file is untracked, unstaged and modified', async () => {
+        await wrapper.setProps({
+          file: {
+            information: {
+              isUntracked: true,
+              isUnstaged: true,
+              hasUnstagedChanged: true,
+            },
+          },
+        });
+
+        expect(wrapper.vm.allowGitAdd).toEqual(true);
+      });
+
+      it('should be true if props.file is untracked', async () => {
+        await wrapper.setProps({
+          file: {
+            information: {
+              isUntracked: true,
+              isUnstaged: false,
+              hasUnstagedChanged: false,
+            },
+          },
+        });
+
+        expect(wrapper.vm.allowGitAdd).toEqual(true);
+      });
+
+      it('should be true if props.file is unstaged', async () => {
+        await wrapper.setProps({
+          file: {
+            information: {
+              isUntracked: false,
+              isUnstaged: true,
+              hasUnstagedChanged: false,
+            },
+          },
+        });
+
+        expect(wrapper.vm.allowGitAdd).toEqual(true);
+      });
+
+      it('should be true if props.file is modified', async () => {
+        await wrapper.setProps({
+          file: {
+            information: {
+              isUntracked: false,
+              isUnstaged: false,
+              hasUnstagedChanged: true,
+            },
+          },
+        });
+
+        expect(wrapper.vm.allowGitAdd).toEqual(true);
+      });
+
+      it('should be false if props.file is staged, unmodified and tracked', async () => {
+        await wrapper.setProps({
+          file: {
+            information: {
+              isUntracked: false,
+              isUnstaged: false,
+              hasUnstagedChanged: false,
+            },
+          },
+        });
+
+        expect(wrapper.vm.allowGitAdd).toEqual(false);
+      });
+
+      it('should be false if props.file.information doesn\'t exist', async () => {
+        await wrapper.setProps({
+          file: {},
+        });
+
+        expect(wrapper.vm.allowGitAdd).toEqual(false);
+      });
+    });
   });
 
   describe('Test functions', () => {
-    describe('Test function: addFile', () => {
+    describe('Test function: createFile', () => {
       it('should call dialog event to create file', () => {
         DialogEvent.next = jest.fn(() => Promise.resolve({
           type: 'open',
@@ -33,8 +170,13 @@ describe('Test component: FileExplorerActionMenu', () => {
           file: wrapper.vm.file,
           isFolder: false,
         }));
-        wrapper.vm.addFile(false);
+        wrapper.vm.menu = {
+          hide: jest.fn(),
+        };
+
+        wrapper.vm.createFile(false);
         expect(DialogEvent.next).toBeCalled();
+        expect(wrapper.vm.menu.hide).toBeCalled();
       });
 
       it('should call dialog event to create folder', () => {
@@ -44,8 +186,13 @@ describe('Test component: FileExplorerActionMenu', () => {
           file: wrapper.vm.file,
           isFolder: true,
         }));
-        wrapper.vm.addFile(true);
+        wrapper.vm.menu = {
+          hide: jest.fn(),
+        };
+
+        wrapper.vm.createFile(true);
         expect(DialogEvent.next).toBeCalled();
+        expect(wrapper.vm.menu.hide).toBeCalled();
       });
     });
 
@@ -56,8 +203,51 @@ describe('Test component: FileExplorerActionMenu', () => {
           key: 'DeleteFile',
           file: wrapper.vm.file,
         }));
+        wrapper.vm.menu = {
+          hide: jest.fn(),
+        };
+
         wrapper.vm.deleteFile();
         expect(DialogEvent.next).toBeCalled();
+        expect(wrapper.vm.menu.hide).toBeCalled();
+      });
+    });
+
+    describe('Test function: addFile', () => {
+      it('should emit an event on success and a notification', async () => {
+        wrapper.vm.loading.add = true;
+        wrapper.vm.menu = {
+          hide: jest.fn(),
+        };
+        Notify.create = jest.fn();
+
+        await wrapper.vm.addFile({ id: 'filePath' });
+
+        expect(Notify.create).toHaveBeenCalledWith(expect.objectContaining({ type: 'positive' }));
+        expect(wrapper.vm.loading.add).toEqual(false);
+        expect(wrapper.vm.menu.hide).toBeCalled();
+      });
+
+      it('should emit a notification on error', async () => {
+        wrapper = shallowMount(FileExplorerActionMenu, {
+          props: {
+            projectName: 'error',
+            file: {
+              id: 'filePath',
+            },
+          },
+        });
+        wrapper.vm.loading.add = true;
+        wrapper.vm.menu = {
+          hide: jest.fn(),
+        };
+        Notify.create = jest.fn();
+
+        await wrapper.vm.addFile({ id: 'filePath' });
+
+        expect(Notify.create).toHaveBeenCalledWith(expect.objectContaining({ type: 'negative' }));
+        expect(wrapper.vm.loading.add).toEqual(false);
+        expect(wrapper.vm.menu.hide).toBeCalled();
       });
     });
   });
