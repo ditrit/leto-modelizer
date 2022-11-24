adaf172ba292644c92caef3a1b43375961e06482
ptanghe <pita8892@gmail.com>
2022-11-24 10:54:25 +0100

    Create SelectInput to manage rules.values

---
+++ src/components/inputs/SelectInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-select
+    clearable
+    v-model="localValue"
+    :options="options"
+    :rules="[
+      (value) => isRequired($t, value, attribute.definition.required),
+    ]"
+  />
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { isRequired } from 'src/composables/QuasarFieldRule';
+
+const props = defineProps({
+  attribute: {
+    type: Object,
+    required: true,
+  },
+});
+
+const localValue = ref(props.attribute.value);
+const options = ref(props.attribute.definition.rules.values);
+</script>
