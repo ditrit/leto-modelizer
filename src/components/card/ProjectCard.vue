5460e187489ab72eb6cab5e500368ee818cd5a5c
Vincent Moittié <moittie.vincent@gmail.com>
2022-11-29 11:30:06 +0100

    Update design of project card

--- src/components/card/ProjectCard.vue
+++ src/components/card/ProjectCard.vue
@@ -9,23 +9,41 @@
       tabindex="0"
       class="cursor-pointer project-card"
     >
-      <q-card-section>
-        <div class="text-h6" data-cy="project-card-title">{{ project.id }}</div>
-      </q-card-section>
+      <q-img :src="getProjectImage()" height="100%">
+        <div class="absolute-bottom text-subtitle2 text-center">
+          {{ project.id }}
+        </div>
+      </q-img>
     </q-card>
   </router-link>
 </template>
 
 <script setup>
-defineProps({
+const props = defineProps({
   project: {
     type: Object,
     required: true,
   },
 });
+
+/**
+ * Get image of project.
+ * Transform project name to a number between 0 and 4 and return associate image.
+ * @return {String} Image path.
+ */
+function getProjectImage() {
+  const number = props.project.id.split('')
+    .map((char) => char.charCodeAt(0))
+    .reduce((acc, value) => acc + value) % 5;
+  return `images/project${number}.png`;
+}
 </script>
 
 <style scoped>
+.project-card {
+  width: 150px;
+  height: 150px;
+}
 .card-link {
   text-decoration: none;
   color: inherit;
