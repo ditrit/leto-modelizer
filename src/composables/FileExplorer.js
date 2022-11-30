e66a7700484bfc6b6aab93fcb3f1ffb69bc67a65
Justine Hell <hell.justine@gmail.com>
2022-11-30 17:12:39 +0100

    Update functions in FileExplorer.js composable

--- src/composables/FileExplorer.js
+++ src/composables/FileExplorer.js
@@ -1,61 +1,55 @@
+import { isParsableFile } from 'src/composables/PluginManager';
+
 /**
  * Create and add a new folder.
- * @param {String} id - Absolute path of folder.
- * @param {Array} folder - Folder that will receive an new empty folder.
+ * @param {String} id - Absolute path of the new folder.
+ * @param {Object} parentFolder - Parent folder.
  * @param {String} name - Name of the new folder.
+ * @return {Object} The created folder.
  */
-export function createFolder(id, folder, name) {
-  folder.push({
+export function createFolder(id, parentFolder, name) {
+  const newFolder = {
     id,
     icon: 'fa-solid fa-folder',
     label: name,
     children: [],
     isFolder: true,
-  });
+    hasParsableFiles: false,
+  };
+
+  parentFolder.children.push(newFolder);
+
+  return newFolder;
 }
 
 /**
  * Create and add a new file.
- * @param {FileInformation} fileInformation - Absolute path of a file.
- * @param {Array} folder - Folder that will receive a new empty file.
+ * @param {FileInformation} fileInformation - Absolute path of the new file.
+ * @param {Object} parentFolder - Parent folder.
  * @param {String} name - Name of the new file.
- * @param {Boolean} [isNewLocalFile=true] - True when the file is locally created otherwise false.
  */
-export function createFile(fileInformation, folder, name, isNewLocalFile = true) {
+export function createFile(fileInformation, parentFolder, name) {
   if (name === '__empty__') {
     return;
   }
-  folder.push({
+  const isParsable = isParsableFile(fileInformation);
+
+  const newFile = {
     id: fileInformation.path,
     icon: 'fa-regular fa-file',
     label: name,
     isFolder: false,
-    isNewLocalFile,
     information: fileInformation,
-  });
+    isParsable,
+  };
+
+  parentFolder.children.push(newFile);
 }
 
 /**
- * Return folder children from a given array of path.
- * @param {Array} folders - Tree folders
- * @param {String[]} paths - Array of path
- * @return {Object} Chosen folder.
- */
-function getTreeFolderChildrenFromPath(folders, paths) {
-  const folder = folders.find((child) => child.label === paths[0]);
-  if (folder) {
-    if (paths.length > 1) {
-      return getTreeFolderChildrenFromPath(folder.children, paths.slice(1));
-    }
-    return folder.children;
-  }
-  return folders;
-}
-
-/**
- * Sort elements by alphabetic order and sort to display folders before files
- * @param {Array} elements - Tree folders
- * @returns {Array} - Sorted Tree
+ * Sort elements by alphabetic order and sort to display folders before files.
+ * @param {Array} elements - Tree folders.
+ * @returns {Array} - Sorted Tree.
  */
 function sortElementsInFolder(elements) {
   return elements.sort((x, y) => {
@@ -70,9 +64,9 @@ function sortElementsInFolder(elements) {
 }
 
 /**
- * Sort Tree root and nested elements
- * @param {Array} elements - Tree folders
- * @returns {Array} - Sorted Tree
+ * Sort Tree root and nested elements.
+ * @param {Array} elements - Tree folders.
+ * @returns {Array} - Sorted Tree.
  */
 export function sortTreeElements(elements) {
   sortElementsInFolder(elements);
@@ -82,49 +76,52 @@ export function sortTreeElements(elements) {
 }
 
 /**
- * Convert fileInformation input to the Object required to display Quasar Tree Component
- * @param {String} projectId - local project id
- * @param {FileInformation[]} fileInformationArray - Array to convert for Quasar Tree Component
- * Example: [{path: '/home/folderA/file1.tf'}, {path: '/home/file2.tf'}]
- * @returns {Array} - The array of nodes that designates the tree structure of Quasar Tree Component
+ * Create file and folder element to build tree based upon path.
+ * @param {FileInformation} fileInformation- Absolute path of tree element.
+ * @param {Object} parentFolder - Parent folder of element to create.
+ * @param {String} path - Path of the tree element.
+ */
+function createTreeElements(fileInformation, parentFolder, path) {
+  const splittedPath = path.split('/').filter(Boolean);
+
+  if (splittedPath.length === 1) {
+    createFile(fileInformation, parentFolder, splittedPath[0]);
+  } else {
+    const folderId = `${fileInformation.path
+      .slice(0, (path.length * -1) + splittedPath[0].length)}`;
+    const folder = parentFolder.children.find((child) => child.label === splittedPath[0])
+      || createFolder(folderId, parentFolder, splittedPath[0]);
+
+    createTreeElements(fileInformation, folder, splittedPath.slice(1).join('/'));
+    folder.hasParsableFiles = folder.children
+      .some((child) => child.hasParsableFiles || child.isParsable);
+  }
+}
+
+/**
+ * Convert fileInformation inputs to the Object required to display Quasar Tree Component.
+ * @param {String} projectId - local project id.
+ * @param {FileInformation[]} fileInformationArray - Array to convert for Quasar Tree Component.
+ * Example: [{path: 'home/folder/file1.tf'}, {path: 'home/file2.tf'}].
+ * @returns {Array} - Array of nodes that designates the tree structure of Quasar Tree Component.
  * @see https://quasar.dev/vue-components/tree
  */
 export function getTree(projectId, fileInformationArray) {
-  const tree = [];
+  const rootFolder = {
+    id: projectId,
+    icon: 'fa-solid fa-folder',
+    label: projectId,
+    isFolder: true,
+    isRootFolder: true,
+    children: [],
+  };
+
   fileInformationArray.forEach((fileInformation) => {
-    const splittedPath = fileInformation.path.split('/').filter(Boolean);
-    splittedPath.forEach((pathName, index) => {
-      const parentPath = splittedPath.slice(0, index);
-      const parentFolderChildren = getTreeFolderChildrenFromPath(tree, parentPath);
-      if (!parentFolderChildren.length
-        || parentFolderChildren.every((child) => child.label !== pathName)) {
-        if (index === splittedPath.length - 1) {
-          createFile(
-            fileInformation,
-            parentFolderChildren,
-            pathName,
-            false,
-          );
-        } else {
-          createFolder(
-            fileInformation.path
-              .substring(0, fileInformation.path.indexOf(pathName) + pathName.length),
-            parentFolderChildren,
-            pathName,
-          );
-        }
-      }
-    });
+    createTreeElements(fileInformation, rootFolder, fileInformation.path);
   });
 
-  return [
-    {
-      id: projectId,
-      icon: 'fa-solid fa-folder',
-      label: projectId,
-      isFolder: true,
-      isRootFolder: true,
-      children: sortTreeElements(tree),
-    },
-  ];
+  return [{
+    ...rootFolder,
+    children: sortTreeElements(rootFolder.children),
+  }];
 }
