ba4c0b48a153a75afaf2269e69973fcb77bd1af2
Justine Hell <hell.justine@gmail.com>
2022-12-16 11:30:06 +0100

    Remove FetchEvent and UpdateRemoteEvent from GitEvent

--- src/composables/events/GitEvent.js
+++ src/composables/events/GitEvent.js
@@ -1,18 +1,6 @@
 import { Subject } from 'rxjs';
 
 /**
- * Represent a rxjs Event object to emit and to receive events about updated remote on git.
- * @typedef {Subject} UpdateRemoteEvent
- */
-const UpdateRemoteEvent = new Subject();
-
-/**
- * Represent a rxjs Event object to emit and to receive events when a fetch is done on git.
- * @typedef {Subject} FetchEvent
- */
-const FetchEvent = new Subject();
-
-/**
  * Represent a rxjs Event object to emit and to receive events when a pull is done on git.
  * @typedef {Subject} FetchEvent
  */
@@ -38,8 +26,6 @@ const NewBranchEvent = new Subject();
 
 export default {
   CheckoutEvent,
-  FetchEvent,
-  UpdateRemoteEvent,
   NewBranchEvent,
   PullEvent,
   PushEvent,
--- tests/unit/composables/events/GitEvent.spec.js
+++ tests/unit/composables/events/GitEvent.spec.js
@@ -2,20 +2,6 @@ import GitEvent from 'src/composables/events/GitEvent';
 import { Subject } from 'rxjs';
 
 describe('Test composable: GitEvent', () => {
-  describe('Test event: UpdateRemoteEvent', () => {
-    it('should export a Subject', () => {
-      expect(GitEvent.UpdateRemoteEvent).toBeDefined();
-      expect(GitEvent.UpdateRemoteEvent).toEqual(new Subject());
-    });
-  });
-
-  describe('Test event: FetchEvent', () => {
-    it('should export a Subject', () => {
-      expect(GitEvent.FetchEvent).toBeDefined();
-      expect(GitEvent.FetchEvent).toEqual(new Subject());
-    });
-  });
-
   describe('Test event: CheckoutEvent', () => {
     it('should export a Subject', () => {
       expect(GitEvent.CheckoutEvent).toBeDefined();
