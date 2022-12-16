cda54b88f8d10f1536f32f311a230e334e50eab2
Justine Hell <hell.justine@gmail.com>
2022-12-16 11:52:10 +0100

    Update DeleteFileForm

--- src/components/form/DeleteFileForm.vue
+++ src/components/form/DeleteFileForm.vue
@@ -33,6 +33,7 @@ import { Notify } from 'quasar';
 import { deleteProjectFile } from 'src/composables/Project';
 import { useI18n } from 'vue-i18n';
 import { ref, computed } from 'vue';
+import FileEvent from 'src/composables/events/FileEvent';
 
 const emit = defineEmits(['file:delete']);
 
@@ -71,6 +72,7 @@ function onSubmit() {
   return deleteProjectFile(props.projectName, props.file.id, props.file.isFolder)
     .then(() => {
       emit('file:delete');
+      FileEvent.DeleteFileEvent.next(props.file);
       Notify.create({
         type: 'positive',
         message: t(`actions.fileExplorer.${props.file.isFolder ? 'folder' : 'file'}.delete`),
