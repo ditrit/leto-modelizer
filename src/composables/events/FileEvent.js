d50cb7d92d79dda6f6a008bc3a74414a60485b58
Justine Hell <hell.justine@gmail.com>
2022-12-16 11:38:05 +0100

    Add SelectFileTabEvent, SelectFileNodeEvent, CreateFileNodeEvent, UpdateEditorContentEvent and UpdateFileContentEvent to FileEvent

--- src/composables/events/FileEvent.js
+++ src/composables/events/FileEvent.js
@@ -3,7 +3,7 @@ import { Subject } from 'rxjs';
 /**
  * Represent a rxjs Event object to emit and to receive events about file creation.
  * The event should be an Object that contains the `name`, `path` and `isFolder`.
- * @typedef {Subject} SelectFileEvent
+ * @typedef {Subject} CreateFileEvent
  */
 const CreateFileEvent = new Subject();
 
@@ -11,7 +11,7 @@ const CreateFileEvent = new Subject();
  * Represent a rxjs Event object to emit and to receive events about node deletion.
  * The event should be an Object that contains `deletedNode` for the node Object to delete
  * and `parentNode` for the deleted node's parent Object.
- * @typedef {Subject} SelectFileEvent
+ * @typedef {Subject} DeleteFileEvent
  */
 const DeleteFileEvent = new Subject();
 
@@ -21,8 +21,48 @@ const DeleteFileEvent = new Subject();
  */
 const GlobalSaveFilesEvent = new Subject();
 
+/**
+ * Represent a rxjs Event object to emit and to receive events about file selection.
+ * The event should be an Object that contains the `id` (path) of the file and `isSelected`.
+ * @typedef {Subject} SelectFileTabEvent
+ */
+const SelectFileTabEvent = new Subject();
+
+/**
+ * Represent a rxjs Event object to emit and to receive events about file node selection.
+ * The event should be a node from the tree.
+ * @typedef {Subject} SelectFileNodeEvent
+ */
+const SelectFileNodeEvent = new Subject();
+
+/**
+ * Represent a rxjs Event object to emit and to receive events about file node creation.
+ * The event should contain the id of the created file node.
+ * @typedef {Subject} CreateFileNodeEvent
+ */
+const CreateFileNodeEvent = new Subject();
+
+/**
+ * Represent a rxjs Event object to emit and to receive events about file node creation. // TODO
+ * The event should contain the id of the created file node.
+ * @typedef {Subject} UpdateEditorContentEvent
+ */
+const UpdateEditorContentEvent = new Subject();
+
+/**
+ * Represent a rxjs Event object to emit and to receive events about file node creation. // TODO
+ * The event should contain the id of the created file node.
+ * @typedef {Subject} UpdateFileContentEvent
+ */
+const UpdateFileContentEvent = new Subject();
+
 export default {
   CreateFileEvent,
   DeleteFileEvent,
   GlobalSaveFilesEvent,
+  SelectFileTabEvent,
+  SelectFileNodeEvent,
+  CreateFileNodeEvent,
+  UpdateEditorContentEvent,
+  UpdateFileContentEvent,
 };
