ac57f19a3f0c4a7388103d0fd1e275200922d851
Justine Hell <hell.justine@gmail.com>
2022-12-16 11:52:43 +0100

    Update GitNewBranchForm

--- tests/unit/components/form/GitNewBranchForm.spec.js
+++ tests/unit/components/form/GitNewBranchForm.spec.js
@@ -2,6 +2,8 @@ import { installQuasarPlugin } from '@quasar/quasar-app-extension-testing-unit-j
 import { shallowMount } from '@vue/test-utils';
 import GitNewBranchForm from 'src/components/form/GitNewBranchForm.vue';
 import { Notify } from 'quasar';
+import GitEvent from 'src/composables/events/GitEvent';
+import Project from 'src/composables/Project';
 
 installQuasarPlugin({
   plugins: [Notify],
@@ -21,12 +23,30 @@ jest.mock('src/composables/Project', () => ({
     }
     return Promise.resolve();
   }),
+  gitCheckout: jest.fn(() => Promise.resolve('checkout')),
+}));
+
+jest.mock('src/composables/events/GitEvent', () => ({
+  NewBranchEvent: {
+    next: jest.fn(),
+  },
+  CheckoutEvent: {
+    next: jest.fn(),
+  },
 }));
 
 describe('Test component: GitNewBranchForm', () => {
   let wrapper;
+  let checkoutNext;
+  let gitCheckoutMock;
 
   beforeEach(() => {
+    checkoutNext = jest.fn();
+    gitCheckoutMock = jest.fn();
+
+    GitEvent.CheckoutEvent.next.mockImplementation(checkoutNext);
+    Project.gitCheckout.mockImplementation(() => Promise.resolve(gitCheckoutMock()));
+
     wrapper = shallowMount(GitNewBranchForm, {
       props: {
         projectName: 'test',
@@ -56,14 +76,33 @@ describe('Test component: GitNewBranchForm', () => {
   });
 
   describe('Test function: onSubmit', () => {
-    it('should emit an event on success and a notification', async () => {
+    it('should emit "git-branch:create" event, emit NewBranchEvent and a notification on success', async () => {
       Notify.create = jest.fn();
 
       await wrapper.vm.onSubmit();
 
       expect(wrapper.vm.submitting).toEqual(false);
       expect(wrapper.emitted()['git-branch:create']).toBeTruthy();
+      expect(GitEvent.NewBranchEvent.next).toHaveBeenCalledTimes(1);
       expect(Notify.create).toHaveBeenCalledWith(expect.objectContaining({ type: 'positive' }));
+    });
+
+    it('should call gitCheckout and emit Checkout event if checkout is true on success', async () => {
+      wrapper.vm.checkout = true;
+
+      await wrapper.vm.onSubmit();
+
+      expect(gitCheckoutMock).toHaveBeenCalled();
+      expect(checkoutNext).toHaveBeenCalledTimes(1);
+    });
+
+    it('should not call gitCheckout nor emit Checkout event if checkout is false on success', async () => {
+      wrapper.vm.checkout = false;
+
+      await wrapper.vm.onSubmit();
+
+      expect(gitCheckoutMock).not.toHaveBeenCalled();
+      expect(checkoutNext).not.toHaveBeenCalled();
     });
 
     it('should emit a notification on error', async () => {
