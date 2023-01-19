242ede99a3f2dbfd2c8f70bc90b79a74b95243d2
Justine Hell <hell.justine@gmail.com>
2023-01-19 15:44:29 +0100

    Create GitAuthenticationDialog

---
+++ src/components/dialog/GitAuthenticationDialog.vue
@@ -0,0 +1,32 @@
+<template>
+  <default-dialog
+    dialog-key="GitAuthentication"
+    data-cy="git-authentication-dialog"
+  >
+    <template v-slot:title>
+      <q-icon
+        color="primary"
+        name="fa-brands fa-git-alt"
+      />
+      {{ $t('page.modelizer.settings.gitAuthentication.title') }}
+    </template>
+    <template v-slot:default>
+      <git-authentication-form
+        :project-name="projectName"
+        @project-git:save="DialogEvent.next({ type: 'close', key: 'GitAuthentication' })"/>
+    </template>
+  </default-dialog>
+</template>
+
+<script setup>
+import DialogEvent from 'src/composables/events/DialogEvent';
+import DefaultDialog from 'components/dialog/DefaultDialog';
+import GitAuthenticationForm from 'components/form/GitAuthenticationForm';
+
+defineProps({
+  projectName: {
+    type: String,
+    required: true,
+  },
+});
+</script>
