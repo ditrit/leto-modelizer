34a3ba067234134901e09b9fd8edbc68a0397c48
Rahzbob <tom.dejardin94@gmail.com>
2023-01-23 12:19:19 +0100

    Create DeleteProjectForm

---
+++ src/components/form/DeleteProjectForm.vue
@@ -0,0 +1,80 @@
+<template>
+  <q-form
+    @submit="onSubmit"
+    class="q-gutter-md delete-project-form"
+    data-cy="delete-project-form"
+  >
+    <q-checkbox
+      v-model="confirmDelete"
+      data-cy="confirm-delete-project-checkbox"
+      :label="$t(
+        'actions.home.deleteProject.confirmDelete',
+        { name: props.projectId },
+      )"
+    />
+    <div class="flex row items-center justify-center">
+      <q-btn
+        :label="$t('actions.home.deleteProject.title')"
+        :loading="submitting"
+        icon="fa-solid fa-trash"
+        type="submit"
+        :disable="!confirmDelete"
+        data-cy="delete-project-form-submit"
+        color="negative"
+      >
+        <template v-slot:loading>
+          <q-spinner-dots />
+        </template>
+      </q-btn>
+    </div>
+  </q-form>
+</template>
+
+<script setup>
+import { Notify } from 'quasar';
+import { ref } from 'vue';
+import { deleteProjectById } from 'src/composables/Project';
+import { useI18n } from 'vue-i18n';
+import ProjectEvent from 'src/composables/events/ProjectEvent';
+
+const emit = defineEmits(['project:delete']);
+
+const props = defineProps({
+  projectId: {
+    type: String,
+    required: true,
+  },
+});
+
+const { t } = useI18n();
+const confirmDelete = ref(false);
+const submitting = ref(false);
+
+/**
+ * Delete project, manage toast and loader.
+ */
+function onSubmit() {
+  submitting.value = true;
+
+  return deleteProjectById(props.projectId)
+    .then(() => {
+      emit('project:delete');
+      ProjectEvent.UpdateProjectEvent.next();
+      Notify.create({
+        type: 'positive',
+        message: t('actions.home.deleteProject.success'),
+        html: true,
+      });
+    })
+    .catch(() => {
+      Notify.create({
+        type: 'negative',
+        message: t('errors.project.delete'),
+        html: true,
+      });
+    })
+    .finally(() => {
+      submitting.value = false;
+    });
+}
+</script>
