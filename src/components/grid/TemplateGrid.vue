48f77d4441c9e18431236a32a4db2f432de69a0b
Justine Hell <hell.justine@gmail.com>
2023-02-09 20:56:51 +0100

    Create TemplateGrid and TemplateCard

---
+++ src/components/grid/TemplateGrid.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="project-grid">
+    <div>
+      <div class="row items-center">
+        <h4>{{ $t('page.home.template.createProject') }}</h4>
+      </div>
+      <div class="row">
+        <TemplateCard
+          v-for="template in templates"
+          :key="template.key"
+          :template="template"
+          class="q-ma-sm"
+          @click="$emit('add:template', template)"
+        />
+      </div>
+      <div
+        v-if="templates.length === 0"
+        class="row text-center text-h6 text-grey empty-grid"
+      >
+        {{ $t('page.home.template.empty') }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import TemplateCard from 'src/components/card/TemplateCard';
+
+defineProps({
+  templates: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.empty-grid {
+  margin-top: 5vh;
+}
+</style>
