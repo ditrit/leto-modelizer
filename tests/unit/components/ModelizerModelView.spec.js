a84e35b32234a4164ae7a7226bd908e71f46aeeb
ptanghe <pita8892@gmail.com>
2023-02-13 10:10:24 +0100

    Update ModelizerModelView: Draw only one model with only one plugin

--- tests/unit/components/ModelizerModelView.spec.js
+++ tests/unit/components/ModelizerModelView.spec.js
@@ -3,8 +3,10 @@ import { shallowMount } from '@vue/test-utils';
 import ModelizerModelView from 'src/components/ModelizerModelView.vue';
 import PluginEvent from 'src/composables/events/PluginEvent';
 import PluginManager from 'src/composables/PluginManager';
+import Project from 'src/composables/Project';
 import TemplateManager from 'src/composables/TemplateManager';
 import { Notify } from 'quasar';
+import { useRoute, useRouter } from 'vue-router';
 
 installQuasarPlugin({
   plugins: [Notify],
@@ -16,6 +18,11 @@ jest.mock('vue-i18n', () => ({
   }),
 }));
 
+jest.mock('vue-router', () => ({
+  useRoute: jest.fn(),
+  useRouter: jest.fn(),
+}));
+
 jest.mock('src/composables/events/PluginEvent', () => ({
   InitEvent: {
     subscribe: jest.fn(),
@@ -24,20 +31,33 @@ jest.mock('src/composables/events/PluginEvent', () => ({
     subscribe: jest.fn(),
   },
   RenderEvent: {
+    next: jest.fn(),
     subscribe: jest.fn(),
   },
   DrawEvent: {
+    subscribe: jest.fn(),
+  },
+  UpdateEvent: {
     subscribe: jest.fn(),
   },
 }));
 
 jest.mock('src/composables/PluginManager', () => ({
   getPlugins: jest.fn(),
-  drawComponents: jest.fn(),
+  deleteComponent: jest.fn(),
+  getPluginByName: jest.fn(),
+  getFileInputs: jest.fn(),
+  renderModel: () => [{ path: 'path' }],
 }));
 
 jest.mock('src/composables/TemplateManager', () => ({
   getTemplatesByType: jest.fn(() => Promise.resolve([{ plugin: 'plugin', isTemplate: true }])),
+}));
+
+jest.mock('src/composables/Project', () => ({
+  getProjectFiles: jest.fn(),
+  readProjectFile: jest.fn(),
+  readDir: jest.fn(),
 }));
 
 describe('Test component: ModelizerModelView', () => {
@@ -50,6 +70,12 @@ describe('Test component: ModelizerModelView', () => {
   let renderUnsubscribe;
   let drawSubscribe;
   let drawUnsubscribe;
+  let updateSubscribe;
+  let updateUnsubscribe;
+  let pluginParse;
+  let pluginDraw;
+  let useRouterPush;
+  let pluginRenderNext;
 
   beforeEach(() => {
     initSubscribe = jest.fn();
@@ -60,6 +86,25 @@ describe('Test component: ModelizerModelView', () => {
     renderUnsubscribe = jest.fn();
     drawSubscribe = jest.fn();
     drawUnsubscribe = jest.fn();
+    updateSubscribe = jest.fn();
+    updateUnsubscribe = jest.fn();
+    pluginParse = jest.fn();
+    pluginDraw = jest.fn();
+    useRouterPush = jest.fn();
+    pluginRenderNext = jest.fn();
+
+    useRoute.mockImplementation(() => ({
+      params: {
+        projectName: 'project-00000000',
+        viewType: 'model',
+      },
+      query: {
+        path: 'path',
+      },
+    }));
+    useRouter.mockImplementation(() => ({
+      push: useRouterPush,
+    }));
 
     PluginEvent.InitEvent.subscribe.mockImplementation(() => {
       initSubscribe();
@@ -77,9 +122,32 @@ describe('Test component: ModelizerModelView', () => {
       drawSubscribe();
       return { unsubscribe: drawUnsubscribe };
     });
+    PluginEvent.UpdateEvent.subscribe.mockImplementation(() => {
+      updateSubscribe();
+      return { unsubscribe: updateUnsubscribe };
+    });
+    PluginEvent.RenderEvent.next.mockImplementation(pluginRenderNext);
+
+    Project.getProjectFiles.mockImplementation(() => Promise.resolve([{}]));
+    Project.readProjectFile.mockImplementation(() => Promise.resolve({ id: 'TEST' }));
+    Project.readDir.mockImplementation(() => Promise.resolve([]));
 
     PluginManager.getPlugins.mockImplementation(() => [{ data: { name: 'pluginName' } }]);
-    PluginManager.drawComponents.mockImplementation(() => 'draw');
+    PluginManager.deleteComponent.mockImplementation((componentId, components) => {
+      const index = components.findIndex(({ id }) => id === componentId);
+      if (index === -1) {
+        return false;
+      }
+      components.splice(index, 1);
+      return true;
+    });
+    PluginManager.getPlugins.mockImplementation(() => []);
+    PluginManager.getPluginByName.mockImplementation(() => ({
+      data: { name: 'pluginName' },
+      parse: pluginParse,
+      draw: pluginDraw,
+    }));
+    PluginManager.getFileInputs.mockImplementation(() => []);
 
     wrapper = shallowMount(ModelizerModelView, {
       props: {
@@ -88,12 +156,19 @@ describe('Test component: ModelizerModelView', () => {
     });
   });
 
+  describe('Test function: renderModelComponents', () => {
+    it('should emit RenderEvent with rendered files', async () => {
+      await wrapper.vm.renderModelComponents();
+
+      expect(pluginRenderNext).toHaveBeenCalledTimes(1);
+    });
+  });
+
   describe('Test function: updatePluginsAndTemplates', () => {
     it('should update data.plugins, call drawComponents and update component templates on success', async () => {
       await wrapper.vm.updatePluginsAndTemplates();
 
-      expect(wrapper.vm.data.plugins).toEqual([{ data: { name: 'pluginName' } }]);
-      expect(PluginManager.drawComponents).toBeCalled();
+      expect(wrapper.vm.data.plugin).toEqual(expect.objectContaining({ data: { name: 'pluginName' } }));
       expect(wrapper.vm.templates).toEqual([{ plugin: 'plugin', isTemplate: true }]);
     });
 
@@ -109,6 +184,46 @@ describe('Test component: ModelizerModelView', () => {
         html: true,
         type: 'negative',
       });
+    });
+  });
+
+  describe('Test function: drawComponents', () => {
+    it('should call parse() & draw() function', async () => {
+      wrapper.vm.data.plugin = {
+        parse: jest.fn(),
+        draw: jest.fn(),
+      };
+
+      await wrapper.vm.drawComponents();
+
+      expect(wrapper.vm.data.plugin.parse).toHaveBeenCalledTimes(1);
+      expect(wrapper.vm.data.plugin.draw).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('Test function: getDirFiles', () => {
+    it('should return an array', async () => {
+      const result = await wrapper.vm.getDirFiles('dir');
+
+      expect(result).toEqual([]);
+    });
+  });
+
+  describe('Test function: onSwitchView', () => {
+    it('should redirect to text view when viewType is "text"', async () => {
+      wrapper.vm.onSwitchView('text');
+      expect(useRouterPush).toHaveBeenCalledWith(expect.objectContaining({
+        name: 'modelizer',
+        params: {
+          viewType: 'text',
+          projectName: 'project-00000000',
+        },
+      }));
+    });
+
+    it('should do nothing otherwise', async () => {
+      wrapper.vm.onSwitchView('notText');
+      expect(useRouterPush).toHaveBeenCalledTimes(0);
     });
   });
 
