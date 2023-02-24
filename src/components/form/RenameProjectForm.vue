3fac4219c2a41ff86b8a280ae727fa8a8b7b13de
Justine Hell <hell.justine@gmail.com>
2023-02-24 17:26:18 +0100

    Create RenameProjectForm.vue

---
+++ src/components/form/RenameProjectForm.vue
@@ -0,0 +1,87 @@
+<template>
+  <q-form
+    @submit="onSubmit"
+    class="q-gutter-md rename-project-form"
+    data-cy="rename-project-form"
+  >
+    <q-input
+      v-model="projectName"
+      :label="$t('page.home.project.name')"
+      :rules="[
+        v => notEmpty($t, v),
+        v => isUniqueProjectName($t, Object.keys(getProjects()), v)
+      ]"
+      filled
+      lazy-rules
+      data-cy="git-project-name-input"
+    />
+    <div class="flex row items-center justify-center">
+      <q-btn
+        :label="$t('actions.home.renameProject.title')"
+        :loading="submitting"
+        icon="fa-solid fa-save"
+        type="submit"
+        data-cy="rename-project-form-submit"
+        color="positive">
+        <template v-slot:loading>
+          <q-spinner-dots/>
+        </template>
+      </q-btn>
+    </div>
+  </q-form>
+</template>
+
+<script setup>
+import { Notify } from 'quasar';
+import { ref } from 'vue';
+import { notEmpty, isUniqueProjectName } from 'src/composables/QuasarFieldRule';
+import { renameProject, getProjects } from 'src/composables/Project';
+import { useI18n } from 'vue-i18n';
+import ProjectEvent from 'src/composables/events/ProjectEvent';
+import DialogEvent from 'src/composables/events/DialogEvent';
+
+const { t } = useI18n();
+const props = defineProps({
+  projectId: {
+    type: String,
+    required: true,
+  },
+});
+
+const projectName = ref(props.projectId);
+const submitting = ref(false);
+
+/**
+ * Rename project, manage toast and loader.
+ */
+function onSubmit() {
+  submitting.value = true;
+
+  return renameProject(props.projectId, projectName.value)
+    .then(() => {
+      DialogEvent.next({ type: 'close', key: 'RenameProject' });
+      ProjectEvent.UpdateProjectEvent.next();
+      Notify.create({
+        type: 'positive',
+        message: t('actions.home.renameProject.success'),
+        html: true,
+      });
+    })
+    .catch(() => {
+      Notify.create({
+        type: 'negative',
+        message: t('errors.renameProject'),
+        html: true,
+      });
+    })
+    .finally(() => {
+      submitting.value = false;
+    });
+}
+</script>
+
+<style lang="scss" scoped>
+.rename-project-form {
+  min-width: 400px;
+}
+</style>
