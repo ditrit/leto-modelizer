2883efb5a6879f247e615387556b33e39d4027ab
Justine Hell <hell.justine@gmail.com>
2023-04-06 16:12:40 +0200

    Delete unused Plugin Events

--- tests/unit/composables/events/PluginEvent.spec.js
+++ tests/unit/composables/events/PluginEvent.spec.js
@@ -9,20 +9,6 @@ describe('Test composable: PluginEvent', () => {
     });
   });
 
-  describe('Test event: EditEvent', () => {
-    it('should export a Subject', () => {
-      expect(PluginEvent.EditEvent).toBeDefined();
-      expect(PluginEvent.EditEvent).toEqual(new Subject());
-    });
-  });
-
-  describe('Test event: UpdateEvent', () => {
-    it('should export a Subject', () => {
-      expect(PluginEvent.UpdateEvent).toBeDefined();
-      expect(PluginEvent.UpdateEvent).toEqual(new Subject());
-    });
-  });
-
   describe('Test event: DefaultEvent', () => {
     it('should export a Subject', () => {
       expect(PluginEvent.DefaultEvent).toBeDefined();
