b79d1e905de360d7aa8791e7caf00436b4063e14
Justine Hell <hell.justine@gmail.com>
2023-05-29 15:02:26 +0200

    Update TemplateGrid

--- src/components/grid/TemplateGrid.vue
+++ src/components/grid/TemplateGrid.vue
@@ -1,26 +1,34 @@
 <template>
-  <div class="project-grid">
-    <div>
-      <div class="row items-center">
+  <q-card
+    class="template-grid bg-white"
+  >
+    <q-card-section>
+      <div class="text-h6 text-secondary">
         <slot name="header" />
       </div>
-      <div class="row">
+    </q-card-section>
+    <q-card-section
+      class="q-ma-md bg-grey-1 q-pb-none template-card-container row items-center wrap"
+    >
+      <div
+        v-for="template in templates"
+        :key="template.key"
+        class="template-card-item row q-mb-md justify-center items-center"
+      >
         <template-card
-          v-for="template in templates"
-          :key="template.key"
           :template="template"
-          class="q-ma-sm"
+          class="template-card"
           @click="$emit('add:template', template)"
         />
       </div>
       <div
         v-if="templates.length === 0"
-        class="row text-center text-h6 text-grey empty-grid"
+        class="row text-center text-subtitle2 text-grey q-mb-md"
       >
         {{ $t('page.home.template.empty') }}
       </div>
-    </div>
-  </div>
+    </q-card-section>
+  </q-card>
 </template>
 
 <script setup>
@@ -37,7 +45,18 @@ defineProps({
 </script>
 
 <style scoped>
-.empty-grid {
-  margin-top: 5vh;
+.template-card-container {
+  overflow-y: auto;
+  max-height: 275px;
+  box-shadow: inset 0px 0px 6px rgba(0, 0, 0, 0.1);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1) !important;
+}
+.template-card-item {
+  flex-basis: calc(100% / 8);
+  min-width: 125px;
+}
+.template-card {
+  width: 100px;
+  height: 100px;
 }
 </style>
