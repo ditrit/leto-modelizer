3357ab1d701a0961b3e6f43881d1c7146dddb536
Justine Hell <hell.justine@gmail.com>
2023-07-11 17:27:17 +0200

    Create LoginLayout and LoginPage

---
+++ src/layouts/LoginLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-layout view="HHh Lpr Fff">
+    <default-navigation-bar />
+    <q-page-container>
+      <login-page />
+    </q-page-container>
+    <q-footer
+      class="bg-primary col"
+    >
+      <q-toolbar class="row justify-center footer">
+        <a
+          :href="$sanitize(`https://github.com/ditrit/leto-modelizer/releases/tag/${version}`)"
+          class="text-white q-ml-md"
+          target="_blank"
+        >
+          {{ $t('footer.version', { version }) }}
+        </a>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script setup>
+import DefaultNavigationBar from 'components/DefaultNavigationBar.vue';
+import LoginPage from 'src/pages/LoginPage';
+import {
+  ref,
+} from 'vue';
+
+const version = ref(process.env.VERSION);
+</script>
