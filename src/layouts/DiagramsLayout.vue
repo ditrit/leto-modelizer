ee17870ef1a85f026bbf5ffe12f719b0a2c14174
X221782 <maxime.vienne-ext@socgen.com>
2023-07-19 08:22:38 +0200

    DiagramsLayout: Use NavigationBar instead of DefaultNavigationBar

--- src/layouts/DiagramsLayout.vue
+++ src/layouts/DiagramsLayout.vue
@@ -1,13 +1,24 @@
 <template>
   <q-layout view="HHh Lpr Fff">
-    <default-navigation-bar />
+    <navigation-bar
+      :project-name="projectName"
+    />
     <q-page-container>
       <diagrams-page />
     </q-page-container>
+    <git-authentication-dialog :project-name="projectName" />
+    <git-add-remote-dialog :project-name="projectName" />
   </q-layout>
 </template>
 
 <script setup>
-import DefaultNavigationBar from 'src/components/DefaultNavigationBar.vue';
+import NavigationBar from 'src/components/NavigationBar.vue';
 import DiagramsPage from 'src/pages/DiagramsPage.vue';
+import GitAuthenticationDialog from 'components/dialog/GitAuthenticationDialog.vue';
+import GitAddRemoteDialog from 'components/dialog/GitAddRemoteDialog.vue';
+import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+
+const route = useRoute();
+const projectName = computed(() => route.params.projectName);
 </script>
