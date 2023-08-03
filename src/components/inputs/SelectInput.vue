db9b5239e3428b6dc57fee74fe241b82654cfb29
Rahzbob <tom.dejardin94@gmail.com>
2023-08-03 11:42:16 +0200

    Update SelectInput

--- src/components/inputs/SelectInput.vue
+++ src/components/inputs/SelectInput.vue
@@ -7,24 +7,83 @@
     :rules="[
       (value) => isRequired($t, value, attribute.definition.required),
     ]"
-  />
+  >
+    <template #option="{ opt }">
+      <item-list
+        :item="opt"
+        @select-item="(value) => localValue = [value]"
+      />
+    </template>
+  </q-select>
 </template>
 
 <script setup>
-import { ref, toRef, watch } from 'vue';
+import {
+  onMounted,
+  ref,
+  toRefs,
+  watch,
+} from 'vue';
 import { isRequired } from 'src/composables/QuasarFieldRule';
+import ItemList from 'components/inputs/ItemList';
 
 const props = defineProps({
   attribute: {
     type: Object,
     required: true,
   },
+  plugin: {
+    type: Object,
+    required: true,
+  },
 });
 
+const emit = defineEmits(['update:model-value']);
+
 const selectInput = ref(null);
-const attribute = toRef(props, 'attribute');
+const { attribute, plugin } = toRefs(props);
 const localValue = ref(attribute.value.value);
-const options = ref(attribute.value.definition.rules.values);
+const options = ref([]);
+const defaultValues = ref(attribute.value.definition.rules.values);
+const variables = ref(plugin.value.data.variables || []);
+
+/**
+ * Initialize the options for the select.
+ */
+function initOptions() {
+  const categories = [...new Set(variables.value.map(({ category }) => category))];
+  const children = categories.map((category) => ({
+    type: 'category',
+    name: category,
+    children: variables.value
+      .filter((variable) => variable.category === category)
+      .map((variable) => ({
+        type: 'item',
+        name: variable.name,
+        value: variable.value !== null ? variable.value : variable.defaultValue,
+        formattedName: variable.formattedName,
+      })),
+  }));
+
+  options.value = [{
+    type: 'category',
+    name: 'default',
+    children: defaultValues.value.map((value) => ({
+      type: 'item',
+      value,
+    })),
+  }, {
+    type: 'category',
+    name: 'variable',
+    children,
+  }];
+}
+
+watch(() => props.plugin.data.components, () => {
+  defaultValues.value = props.plugin.data.getComponentsByType(
+    props.attribute.definition.linkRef,
+  ).map(({ id }) => id);
+});
 
 watch(() => selectInput.value, () => {
   if (selectInput.value) {
@@ -33,10 +92,20 @@ watch(() => selectInput.value, () => {
 });
 
 watch(() => props.attribute, () => {
-  localValue.value = props.attribute.value;
+  if (props.attribute.value) {
+    localValue.value = props.attribute.value;
+  }
 
   if (selectInput.value) {
     selectInput.value.validate();
   }
+});
+
+watch(() => localValue.value, () => {
+  emit('update:model-value', localValue.value);
+});
+
+onMounted(() => {
+  initOptions();
 });
 </script>
--- tests/unit/components/inputs/SelectInput.spec.js
+++ tests/unit/components/inputs/SelectInput.spec.js
@@ -3,12 +3,49 @@ import { shallowMount } from '@vue/test-utils';
 import SelectInput from 'src/components/inputs/SelectInput';
 import { createI18n } from 'vue-i18n';
 import i18nConfiguration from 'src/i18n';
+import { ComponentAttributeDefinition } from 'leto-modelizer-plugin-core';
 
 installQuasarPlugin();
 
 describe('Test component: SelectInput', () => {
   let wrapper;
 
+  const options = [{
+    name: 'default',
+    type: 'category',
+    children: [{
+      type: 'item',
+      value: 'value1',
+    }, {
+      type: 'item',
+      value: 'value2',
+    }],
+  }, {
+    name: 'variable',
+    type: 'category',
+    children: [{
+      name: 'variable',
+      type: 'category',
+      children: [{
+        name: 'variableName',
+        formattedName: 'var.variableName',
+        value: 'variableValue',
+        type: 'item',
+      }],
+    }],
+  }];
+  const qSelectStub = {
+    template: `
+      <div>
+        <slot name="prepend"></slot>
+        <slot></slot>
+      </div>
+    `,
+    props: ['value', 'options', 'rules'],
+    methods: {
+      validate: jest.fn(),
+    },
+  };
   beforeEach(() => {
     wrapper = shallowMount(SelectInput, {
       props: {
@@ -24,10 +61,20 @@ describe('Test component: SelectInput', () => {
             },
           },
         },
+        plugin: {
+          data: {
+            variables: [{
+              category: 'variable',
+              name: 'variableName',
+              formattedName: 'var.variableName',
+              value: 'variableValue',
+            }],
+          },
+        },
       },
       global: {
         stubs: {
-          qSelect: true,
+          qSelect: qSelectStub,
         },
         plugins: [
           createI18n({
@@ -56,14 +103,47 @@ describe('Test component: SelectInput', () => {
         expect(wrapper.vm.localValue).toEqual('value1');
       });
     });
+  });
 
-    describe('Test variable: option', () => {
-      it('should match attribute.definition.rules.values', () => {
-        expect(wrapper.vm.options).toEqual([
-          'value1',
-          'value2',
-        ]);
+  describe('Test function: initOptions', () => {
+    it('should init options', () => {
+      expect(wrapper.vm.options).toEqual(options);
+    });
+  });
+
+  describe('Test watcher: props.plugin.data.components', () => {
+    it('should update options', async () => {
+      await wrapper.setProps({
+        attribute: {
+          value: ['test'],
+          name: 'attributeName',
+          definition: new ComponentAttributeDefinition(),
+        },
+        plugin: {
+          data: {
+            getComponentsByType: jest.fn(() => [{ id: 'componentName' }]),
+            name: 'pluginName',
+            components: [{
+              name: 'componentName',
+            }],
+          },
+        },
       });
+
+      expect(wrapper.vm.options).toEqual(options);
+    });
+  });
+
+  describe('Test watcher: props.attributes', () => {
+    it('should update localValue when props.attribute.value is defined', async () => {
+      await wrapper.setProps({
+        attribute: {
+          value: ['updatedValue'],
+          name: 'attributeName',
+        },
+      });
+
+      expect(wrapper.vm.localValue).toEqual(['updatedValue']);
     });
   });
 });
