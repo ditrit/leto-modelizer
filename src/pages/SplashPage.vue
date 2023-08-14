1b5f4fc3890d686b88253ef31ee5ce0acbdf6e65
Vincent Moittié <moittie.vincent@gmail.com>
2023-08-14 15:32:28 +0200

    Add splash screen

---
+++ src/pages/SplashPage.vue
@@ -0,0 +1,62 @@
+<template>
+  <q-page
+    class="splash-page flex justify-center items-center"
+    data-cy="splash-page"
+  >
+    <q-card class="bg-primary shadow-2">
+      <q-card-section class="flex column justify-center items-center text-white">
+        <div class="flex items-center text-h5">
+          <q-img class="logo" />
+          <label>{{ $t('application.name') }}</label>
+        </div>
+        <div class="q-pt-md text-bold">
+          {{ $t('page.splash.wait') }}
+        </div>
+        <div class="text-bold q-mb-md">
+          {{ $t('page.splash.info') }}
+        </div>
+        <q-spinner-gears
+          color="white"
+          size="4rem"
+          indeterminate
+        />
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router';
+import { onMounted } from 'vue';
+import { initPlugins } from 'src/composables/PluginManager';
+import { setUserManager } from 'src/composables/UserAuthentication';
+import PluginEvent from 'src/composables/events/PluginEvent';
+
+const route = useRoute();
+const router = useRouter();
+
+onMounted(async () => {
+  await initPlugins();
+  PluginEvent.InitEvent.next();
+  setUserManager(localStorage.getItem('provider'));
+
+  // Wait 2s to avoid blinking effect and let user admire our beautiful splash screen.
+  // eslint-disable-next-line no-promise-executor-return
+  await new Promise((res) => setTimeout(res, 2000));
+
+  router.push(route.query.from === route.path ? { name: 'Home' } : route.query.from || { name: 'Home' });
+});
+</script>
+
+<style lang="scss" scoped>
+.splash-page {
+  background-image: url('/splash.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.logo {
+  width: 100px;
+  height: 100px;
+  margin: 0;
+}
+</style>
---
+++ tests/unit/pages/SplashPage.spec.js
@@ -0,0 +1,76 @@
+import { installQuasarPlugin } from '@quasar/quasar-app-extension-testing-unit-jest';
+import { shallowMount } from '@vue/test-utils';
+import SplashPage from 'src/pages/SplashPage.vue';
+import PluginEvent from 'src/composables/events/PluginEvent';
+import PluginManager from 'src/composables/PluginManager';
+import UserAuthentication from 'src/composables/UserAuthentication';
+
+installQuasarPlugin();
+
+jest.mock('src/composables/UserAuthentication', () => ({
+  setUserManager: jest.fn(),
+}));
+
+jest.mock('src/composables/PluginManager', () => ({
+  initPlugins: () => Promise.resolve(),
+  getPlugins: jest.fn(),
+}));
+
+jest.mock('src/composables/events/PluginEvent', () => ({
+  InitEvent: {
+    next: jest.fn(),
+  },
+}));
+
+jest.mock('vue-router', () => ({
+  useRoute: () => ({
+    query: {
+      from: 'test',
+    },
+  }),
+  useRouter: () => ({
+    push: () => {},
+    path: 'test',
+  }),
+}));
+
+jest.useFakeTimers();
+jest.spyOn(global, 'setTimeout');
+
+describe('Test component: SplashPage', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    PluginManager.getPlugins.mockImplementation(() => [
+      { data: { deleteAllEventLogsBefore: jest.fn(() => {}) } },
+    ]);
+
+    wrapper = shallowMount(SplashPage, {
+      global: {
+        stubs: {
+          qIcon: true,
+        },
+      },
+    });
+  });
+
+  describe('Test function: onMounted', () => {
+    it('should call PluginEvent.InitEvent', async () => {
+      jest.advanceTimersByTime(5000);
+      await wrapper.vm.$nextTick();
+      expect(wrapper.exists()).toBe(true);
+      expect(PluginEvent.InitEvent.next).toHaveBeenCalled();
+    });
+
+    it('should call setTimeout', async () => {
+      await wrapper.vm.$nextTick();
+      expect(setTimeout).toHaveBeenCalled();
+      expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 2000);
+    });
+
+    it('should call setUserManager', async () => {
+      await wrapper.vm.$nextTick();
+      expect(UserAuthentication.setUserManager).toHaveBeenCalled();
+    });
+  });
+});
