5b0796eb0c589d6e8a00893b80e056716dbcc293
Justine Hell <hell.justine@gmail.com>
2023-09-28 14:26:49 +0200

    Add tests to improve coverage

--- tests/unit/pages/ModelizerDrawPage.spec.js
+++ tests/unit/pages/ModelizerDrawPage.spec.js
@@ -1,8 +1,15 @@
 import { installQuasarPlugin } from '@quasar/quasar-app-extension-testing-unit-jest';
 import { shallowMount } from '@vue/test-utils';
+import { Notify } from 'quasar';
 import ModelizerDrawPage from 'src/pages/ModelizerDrawPage.vue';
+import PluginManager from 'src/composables/PluginManager';
+import TemplateManager from 'src/composables/TemplateManager';
+import PluginEvent from 'src/composables/events/PluginEvent';
 
-installQuasarPlugin();
+installQuasarPlugin({
+  plugins: [Notify],
+});
+
 jest.mock('vue-i18n', () => ({
   useI18n: () => ({
     t: (t) => t,
@@ -39,12 +46,80 @@ jest.mock('src/composables/PluginManager', () => ({
   })),
   initComponents: jest.fn(() => Promise.resolve()),
   renderConfiguration: jest.fn(() => Promise.resolve()),
+  renderModel: jest.fn(() => Promise.resolve()),
+  addNewComponent: jest.fn(() => Promise.resolve()),
+  addNewTemplateComponent: jest.fn(() => Promise.resolve()),
 }));
+
+jest.mock('src/composables/TemplateManager', () => ({
+  getTemplatesByType: jest.fn(() => Promise.resolve()),
+}));
+
+jest.mock('src/composables/events/PluginEvent', () => ({
+  DefaultEvent: {
+    subscribe: jest.fn(),
+  },
+}));
+
 describe('Test page component: ModelizerDrawPage', () => {
   let wrapper;
+  let subscribe;
+  let unsubscribe;
 
   beforeEach(() => {
+    subscribe = jest.fn();
+    unsubscribe = jest.fn();
+
+    PluginEvent.DefaultEvent.subscribe.mockImplementation(() => {
+      subscribe();
+      return { unsubscribe };
+    });
+
     wrapper = shallowMount(ModelizerDrawPage);
+  });
+
+  describe('Test function: onDefaultEvent', () => {
+    it('should call renderConfiguration if event.action is "move"', async () => {
+      const param = {
+        event: {
+          type: 'Drawer', action: 'move',
+        },
+      };
+
+      await wrapper.vm.onDefaultEvent(param);
+
+      expect(PluginManager.renderConfiguration).toBeCalledWith('project-00000000', 'path', wrapper.vm.data.plugin);
+    });
+
+    it('should call renderModel if event.action is either "update", "delete" or "add"', async () => {
+      let param = {
+        event: {
+          type: 'Drawer', action: 'update',
+        },
+      };
+
+      await wrapper.vm.onDefaultEvent(param);
+
+      expect(PluginManager.renderModel).toBeCalledWith('project-00000000', 'path', wrapper.vm.data.plugin);
+
+      param = {
+        event: {
+          type: 'Drawer', action: 'delete',
+        },
+      };
+      await wrapper.vm.onDefaultEvent(param);
+
+      expect(PluginManager.renderModel).toBeCalledWith('project-00000000', 'path', wrapper.vm.data.plugin);
+
+      param = {
+        event: {
+          type: 'Drawer', action: 'add',
+        },
+      };
+      await wrapper.vm.onDefaultEvent(param);
+
+      expect(PluginManager.renderModel).toBeCalledWith('project-00000000', 'path', wrapper.vm.data.plugin);
+    });
   });
 
   describe('Test function: arrangeComponentsPosition', () => {
@@ -53,6 +128,86 @@ describe('Test page component: ModelizerDrawPage', () => {
 
       expect(wrapper.vm.data.plugin.arrangeComponentsPosition).toHaveBeenCalled();
       expect(wrapper.vm.data.plugin.draw).toHaveBeenCalledWith('root');
+    });
+  });
+
+  describe('Test function: initView', () => {
+    it('should call draw with "root" parameter', async () => {
+      const initComponentsMock = jest.fn(() => Promise.resolve());
+
+      PluginManager.initComponents.mockImplementation(initComponentsMock);
+      expect(initComponentsMock).toHaveBeenCalledTimes(0);
+
+      await wrapper.vm.initView();
+
+      expect(wrapper.vm.data.plugin.draw).toHaveBeenCalledWith('root');
+      expect(PluginManager.initComponents).toBeCalled();
+    });
+
+    it('should emit a notification on error when updating component templates', async () => {
+      Notify.create = jest.fn();
+      TemplateManager.getTemplatesByType.mockImplementation(() => Promise.reject());
+
+      await wrapper.vm.initView();
+
+      expect(Notify.create).toHaveBeenCalledWith(expect.objectContaining({ type: 'negative' }));
+    });
+  });
+
+  describe('Test function: dropHandler', () => {
+    it('should call addNewComponent if component is not a template', async () => {
+      const param = {
+        dataTransfer: {
+          getData: jest.fn(() => ('{"isTemplate":false,"definitionType":"testComponent"}')),
+        },
+      };
+
+      await wrapper.vm.dropHandler(param);
+
+      expect(PluginManager.addNewComponent).toBeCalled();
+      expect(PluginManager.renderModel).toBeCalledWith('project-00000000', 'path', wrapper.vm.data.plugin);
+    });
+
+    it('should call addNewTemplateComponent if component is a template', async () => {
+      const param = {
+        dataTransfer: {
+          getData: jest.fn(() => ('{"isTemplate":true,"definitionType":"testComponent"}')),
+        },
+      };
+
+      await wrapper.vm.dropHandler(param);
+
+      expect(PluginManager.addNewTemplateComponent).toBeCalled();
+      expect(PluginManager.renderModel).toBeCalledWith('project-00000000', 'path', wrapper.vm.data.plugin);
+    });
+
+    it('should emit a notification on error when adding a new template component', async () => {
+      Notify.create = jest.fn();
+      PluginManager.addNewTemplateComponent.mockImplementation(() => Promise.reject());
+
+      const param = {
+        dataTransfer: {
+          getData: jest.fn(() => ('{"isTemplate":true,"definitionType":"testComponent"}')),
+        },
+      };
+
+      await wrapper.vm.dropHandler(param);
+
+      expect(Notify.create).toHaveBeenCalledWith(expect.objectContaining({ type: 'negative' }));
+    });
+  });
+
+  describe('Test hook function: onMounted', () => {
+    it('should subscribe DefaultEvent', () => {
+      expect(subscribe).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('Test hook function: onUnmounted', () => {
+    it('should unsubscribe DefaultEvent', () => {
+      expect(unsubscribe).toHaveBeenCalledTimes(0);
+      wrapper.unmount();
+      expect(unsubscribe).toHaveBeenCalledTimes(1);
     });
   });
 });
