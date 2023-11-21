ace03efa243883c37fdfae3ff6e056ccaac0d7da
Justine Hell <hell.justine@gmail.com>
2023-11-21 10:56:54 +0100

    Remove all git related function in Project.js

--- tests/unit/composables/Project.spec.js
+++ tests/unit/composables/Project.spec.js
@@ -7,13 +7,6 @@ import {
   initProject,
   getProjectFiles,
   readProjectFile,
-  gitAddRemote,
-  getCurrentBranch,
-  gitFetch,
-  getBranches,
-  gitCheckout,
-  createBranchFrom,
-  gitUpdate,
   createProjectFolder,
   writeProjectFile,
   appendProjectFile,
@@ -23,15 +16,6 @@ import {
   deleteProjectDir,
   deleteProjectFile,
   deleteDiagramFile,
-  getStatus,
-  gitListFiles,
-  gitPush,
-  gitAdd,
-  gitRemove,
-  gitCommit,
-  gitLog,
-  gitGlobalUpload,
-  importProject,
   PROJECT_STORAGE_KEY,
   getAllModels,
   getModelFiles,
@@ -41,60 +25,13 @@ import {
   extractProjectName,
   getProjectFolders,
 } from 'src/composables/Project';
+import {
+  gitInit,
+  gitAdd,
+  gitCommit,
+} from 'src/composables/Git';
 import { FileInformation, FileInput } from 'leto-modelizer-plugin-core';
-import Branch from 'src/models/git/Branch';
-import git from 'isomorphic-git';
-import FileStatus from 'src/models/git/FileStatus';
 import Project from 'src/models/Project';
-
-jest.mock('isomorphic-git', () => ({
-  init: jest.fn(() => Promise.resolve('init')),
-  clone: jest.fn(({ onAuth }) => {
-    onAuth();
-
-    return Promise.resolve('clone');
-  }),
-  branch: jest.fn(({ ref }) => {
-    if (ref === 'error') {
-      return Promise.reject({ message: 'ERROR' });
-    }
-
-    if (ref === 'enotdir') {
-      return Promise.reject({ message: 'ENOTDIR: File is not a directory.' });
-    }
-
-    return Promise.resolve('branch');
-  }),
-  addRemote: jest.fn(() => Promise.resolve('addRemote')),
-  fetch: jest.fn(),
-  checkout: jest.fn(() => Promise.resolve('checkout')),
-  listFiles: jest.fn(() => Promise.resolve(['/test/file.txt'])),
-  listBranches: jest.fn(({ remote }) => {
-    if (!remote) {
-      return Promise.resolve(['HEAD', 'main', 'local']);
-    }
-
-    return Promise.resolve(['HEAD', 'main', 'remote']);
-  }),
-  resolveRef: jest.fn(() => Promise.resolve('resolveRef')),
-  readBlob: jest.fn(() => Promise.resolve({ blob: 'test' })),
-  currentBranch: jest.fn(() => Promise.resolve('main')),
-  pull: jest.fn(({ onAuth }) => {
-    onAuth();
-
-    return Promise.resolve('pull');
-  }),
-  statusMatrix: jest.fn(() => Promise.resolve([['test', 0, 1, 2]])),
-  push: jest.fn(({ onAuth }) => {
-    onAuth();
-
-    return Promise.resolve('pull');
-  }),
-  add: jest.fn(() => Promise.resolve('add')),
-  remove: jest.fn(() => Promise.resolve('remove')),
-  commit: jest.fn(() => Promise.resolve('SHA-1')),
-  log: jest.fn(() => Promise.resolve(['log'])),
-}));
 
 jest.mock('browserfs', () => ({
   install: jest.fn(),
@@ -166,8 +103,6 @@ jest.mock('browserfs', () => ({
 jest.mock('src/composables/PluginManager', () => ({
   getFileInputs: () => [],
   getPlugins: () => [],
-  getPluginTags: () => [],
-  isParsableFile: () => true,
   getPluginByName: jest.fn(() => ({
     isParsable: () => true,
     configuration: {
@@ -176,27 +111,17 @@ jest.mock('src/composables/PluginManager', () => ({
   })),
 }));
 
-describe('Test composable: Project', () => {
-  let gitAddMock;
-  let gitRemoveMock;
-  let gitAddRemoteMock;
-  let gitFetchMock;
+jest.mock('src/composables/Git', () => ({
+  gitListFiles: () => [],
+  gitRemove: () => Promise.resolve(),
+  gitInit: jest.fn(() => Promise.resolve()),
+  gitAdd: jest.fn(() => Promise.resolve()),
+  gitCommit: jest.fn(() => Promise.resolve()),
+}));
 
+describe('Test composable: Project', () => {
   beforeEach(() => {
     localStorage.clear();
-    gitAddMock = jest.fn();
-    gitRemoveMock = jest.fn();
-    gitAddRemoteMock = jest.fn();
-    gitFetchMock = jest.fn(({ onAuth }) => {
-      onAuth();
-
-      return Promise.resolve('fetch');
-    });
-
-    git.add.mockImplementation(gitAddMock);
-    git.remove.mockImplementation(gitRemoveMock);
-    git.fetch.mockImplementation(gitFetchMock);
-    git.addRemote.mockImplementation(gitAddRemoteMock);
   });
 
   describe('Test function: getProjects', () => {
@@ -305,24 +230,15 @@ describe('Test composable: Project', () => {
 
   describe('Test function: initProject', () => {
     it('should init and create commit with default file', async () => {
-      await initProject({ id: 'foo' });
-      expect(git.init).toBeCalled();
-      expect(git.add).toBeCalled();
-      expect(git.commit).toBeCalled();
-    });
-  });
+      gitInit.mockResolvedValueOnce();
+      gitAdd.mockResolvedValueOnce();
+      gitCommit.mockResolvedValueOnce();
 
-  describe('Test function: importProject', () => {
-    it('should call git clone', async () => {
-      await importProject({
-        id: 'foo',
-        git: {
-          repository: 'test',
-          username: 'test',
-          token: 'test',
-        },
-      });
-      expect(git.clone).toBeCalled();
+      await initProject({ id: 'foo' });
+
+      expect(gitInit).toHaveBeenCalledWith('foo');
+      expect(gitAdd).toHaveBeenCalledWith('foo', 'README.md');
+      expect(gitCommit).toHaveBeenCalledWith('foo', 'Initial commit.');
     });
   });
 
@@ -396,55 +312,6 @@ describe('Test composable: Project', () => {
     });
   });
 
-  describe('Test function: gitAddRemote', () => {
-    it('should call all needed git method', async () => {
-      localStorage.setItem(PROJECT_STORAGE_KEY, JSON.stringify({
-        test: {
-          id: 'test',
-          git: {
-            repository: 'test',
-            username: 'test',
-            token: 'test',
-          },
-        },
-      }));
-      await gitAddRemote({
-        id: 'test',
-        git: {
-          repository: 'test',
-          username: 'test',
-          token: 'test',
-        },
-      });
-
-      expect(gitAddRemoteMock).toBeCalled();
-    });
-  });
-
-  describe('Test function: gitFetch', () => {
-    it('should not call git.fetch', async () => {
-      const result = await gitFetch({});
-
-      expect(gitFetchMock).not.toBeCalled();
-      expect(result).toBeUndefined();
-    });
-
-    it('should call git.fetch', async () => {
-      const result = await gitFetch({ git: { repository: 'test' } });
-
-      expect(gitFetchMock).toBeCalled();
-      expect(result).toEqual('fetch');
-    });
-  });
-
-  describe('Test function: gitCheckout', () => {
-    it('should emit checkout event', async () => {
-      const result = await gitCheckout('projectId', 'test').then(() => 'success');
-
-      expect(result).toEqual('success');
-    });
-  });
-
   describe('Test function: getProjectFiles', () => {
     it('should return file information array', async () => {
       localStorage.setItem(PROJECT_STORAGE_KEY, JSON.stringify({
@@ -485,79 +352,6 @@ describe('Test composable: Project', () => {
       const result = await readProjectFile('test', new FileInformation({ path: '/test/file.txt' }));
 
       expect(result).toEqual(new FileInput({ path: '/test/file.txt', content: 'test' }));
-    });
-  });
-
-  describe('Test function: getCurrentBranch', () => {
-    it('should return current branch name', async () => {
-      const result = await getCurrentBranch('test');
-
-      expect(result).toEqual('main');
-    });
-  });
-
-  describe('Test function: getBranches', () => {
-    it('should return valid branches', async () => {
-      const branches = await getBranches('test');
-
-      expect(branches).toEqual([
-        new Branch({
-          name: 'main',
-          onLocal: true,
-          onRemote: true,
-          remote: 'origin',
-        }),
-        new Branch({
-          name: 'local',
-          onLocal: true,
-          onRemote: false,
-          remote: 'origin',
-        }),
-        new Branch({
-          name: 'remote',
-          onLocal: false,
-          onRemote: true,
-          remote: 'origin',
-        }),
-      ]);
-    });
-  });
-
-  describe('Test function: createBranchFrom', () => {
-    it('should succeed when there is no issue', async () => {
-      const result = await createBranchFrom('test', 'branch', 'main').then(() => 'success');
-
-      expect(result).toEqual('success');
-    });
-
-    it('should fail in case of ENOTDIR', async () => {
-      const error = await createBranchFrom('test', 'enotdir', 'main').catch(({ message }) => message);
-
-      expect(error).toEqual('ENOTDIR: File is not a directory.');
-    });
-
-    it('should fail in case of ERROR', async () => {
-      const error = await createBranchFrom('test', 'error', 'main').catch(({ message }) => message);
-
-      expect(error).toEqual('ERROR');
-    });
-  });
-
-  describe('Test function: gitUpdate', () => {
-    it('should call git pull', async () => {
-      await gitUpdate(
-        {
-          id: 'test',
-          git: {
-            username: 'username',
-            token: 'token',
-          },
-        },
-        'branch',
-        true,
-      );
-
-      expect(git.pull).toBeCalled();
     });
   });
 
@@ -614,98 +408,6 @@ describe('Test composable: Project', () => {
       const result = await deleteDiagramFile('test', 'container/parent').then(() => 'success');
 
       expect(result).toEqual('success');
-    });
-  });
-
-  describe('Test function: getStatus', () => {
-    it('should be a success and return an array with one valid FileStatus', async () => {
-      expect(await getStatus()).toEqual([new FileStatus({
-        path: 'test',
-        headStatus: 0,
-        workdirStatus: 1,
-        stageStatus: 2,
-      })]);
-    });
-  });
-
-  describe('Test function: gitListFiles', () => {
-    it('should be a success and return an array with list of filePaths', async () => {
-      expect(await gitListFiles()).toEqual(['/test/file.txt']);
-    });
-  });
-
-  describe('Test function: gitPush', () => {
-    it('should call git push and emit event', async () => {
-      await gitPush(
-        {
-          id: 'test',
-          git: {
-            username: 'username',
-            token: 'token',
-          },
-        },
-        'branch',
-        true,
-      );
-
-      expect(git.push).toBeCalled();
-    });
-  });
-
-  describe('Test function: gitAdd', () => {
-    it('should call git add', async () => {
-      await gitAdd('projectId', 'filepath');
-
-      expect(gitAddMock).toBeCalled();
-    });
-  });
-
-  describe('Test function: gitRemove', () => {
-    it('should call git remove', async () => {
-      await gitRemove('projectId', 'filepath');
-
-      expect(gitRemoveMock).toBeCalled();
-    });
-  });
-
-  describe('Test function: gitCommit', () => {
-    it('should call git commit and return SHA-1', async () => {
-      let result = await gitCommit('test', 'wip');
-      expect(result).toEqual('SHA-1');
-
-      result = await gitCommit('test', 'wip', true);
-      expect(result).toEqual('SHA-1');
-    });
-  });
-
-  describe('Test function: gitGlobalUpload', () => {
-    it('should succeed', async () => {
-      const result = await gitGlobalUpload({
-        git: {
-          username: 'username',
-          token: 'token',
-        },
-      }).then(() => 'success');
-
-      expect(result).toEqual('success');
-    });
-  });
-
-  describe('Test function: gitLog', () => {
-    it('should return valid log', async () => {
-      const result = await gitLog('test', 'main');
-
-      expect(result).toEqual(['log']);
-    });
-
-    it('should call git log with default depth', async () => {
-      await gitLog('test', 'main');
-      expect(git.log).toBeCalledWith(expect.objectContaining({ depth: 25 }));
-    });
-
-    it('should call git log with specified depth', async () => {
-      await gitLog('test', 'main', 1);
-      expect(git.log).toBeCalledWith(expect.objectContaining({ depth: 1 }));
     });
   });
 
