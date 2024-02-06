7f8a14c9ce1df9446008ca56d941e8321a1beb79
Aboubakar <deathmaskk1234@gmail.com>
2024-02-06 19:40:31 +0100

    Add api in boot/axios

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -5,9 +5,24 @@ const templateLibraryApiClient = axios.create({
   timeout: 15000,
 });
 
+const api = axios.create({
+  baseURL: '/api',
+  timeout: 15000,
+});
+
 templateLibraryApiClient.interceptors.response.use(
   (response) => Promise.resolve(response),
   (error) => Promise.reject(error),
 );
 
-export { axios, templateLibraryApiClient };
+api.interceptors.response.use(
+  ({ data }) => Promise.resolve(data),
+  (error) => {
+    if (error.response.status === 401) {
+      window.location.href = `${process.env.BACKEND_URL}/api/login`;
+    }
+    return Promise.reject(error);
+  },
+);
+
+export { api, templateLibraryApiClient };
