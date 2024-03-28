b3f350324218a8cd7c76c9ef38c24214e0381298
Vincent Moittié <moittie.vincent@gmail.com>
2024-03-28 14:47:34 +0100

    Add dialog with form to manage ia request

---
+++ src/components/dialog/CreateIAModelDialog.vue
@@ -0,0 +1,31 @@
+<template>
+  <default-dialog
+    dialog-key="CreateIAModel"
+    data-cy="create-ia-model-dialog"
+  >
+    <template #title>
+      <q-icon
+        color="primary"
+        name="fa-solid fa-scroll"
+      />
+      {{ $t(`actions.models.create.dialog.name`) }}
+    </template>
+    <template #default>
+      <create-ia-model-form
+        :project-name="projectName"
+      />
+    </template>
+  </default-dialog>
+</template>
+
+<script setup>
+import DefaultDialog from 'components/dialog/DefaultDialog.vue';
+import CreateIaModelForm from 'components/form/CreateIAModelForm.vue';
+
+defineProps({
+  projectName: {
+    type: String,
+    required: true,
+  },
+});
+</script>
