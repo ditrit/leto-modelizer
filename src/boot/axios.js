fe7f7b965356bc64a328218e4c8d09a6a9624682
Vincent Moittié <moittie.vincent@gmail.com>
2024-03-28 14:46:58 +0100

    Setup csrf for api

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useCsrfStore } from 'src/stores/CsrfTokenStore';
 
 const templateLibraryApiClient = axios.create({
   baseURL: '/template-library/',
@@ -15,6 +16,22 @@ templateLibraryApiClient.interceptors.response.use(
   (error) => Promise.reject(error),
 );
 
+api.interceptors.request.use(
+  async (config) => {
+    if (['post', 'put', 'delete'].includes(config.method)) {
+      const {
+        token,
+        headerName,
+      } = useCsrfStore();
+
+      config.headers[headerName] = token;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
 api.interceptors.response.use(
   ({ data }) => Promise.resolve(data),
   (error) => {
@@ -25,4 +42,27 @@ api.interceptors.response.use(
   },
 );
 
-export { api, templateLibraryApiClient };
+/**
+ * Asynchronously prepares a request by ensuring the availability of a valid CSRF token.
+ *
+ * This function uses a CSRF token to check if token is valid.
+ * If not, it fetches a new CSRF token from the server using the provided API.
+ * The retrieved CSRF token is then stored in the CSRF token store for future use.
+ * @returns {Promise<object>} The API instance with an updated CSRF token.
+ */
+async function prepareApiRequest() {
+  const csrfStore = useCsrfStore();
+  const currentTime = new Date().getTime();
+
+  if (!csrfStore.expirationDate || csrfStore.expirationDate < currentTime) {
+    const csrf = await api.get('/csrf');
+
+    csrfStore.headerName = csrf.headerName;
+    csrfStore.token = csrf.token;
+    csrfStore.expirationDate = csrf.expirationDate;
+  }
+
+  return api;
+}
+
+export { api, prepareApiRequest, templateLibraryApiClient };
