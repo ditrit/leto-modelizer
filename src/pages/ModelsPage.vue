9792a569e06def49c6355d51af6ccadf08ef8fb9
Vincent Moittié <moittie.vincent@gmail.com>
2024-03-28 14:48:05 +0100

    Add button to create a diagram from AI

--- src/pages/ModelsPage.vue
+++ src/pages/ModelsPage.vue
@@ -5,6 +5,7 @@
     />
 
     <create-model-dialog :project-name="projectName" />
+    <create-a-i-model-dialog :project-name="projectName" />
     <delete-model-dialog :project-name="projectName" />
     <rename-model-dialog :project-name="projectName" />
     <import-model-template-dialog :project-name="projectName" />
@@ -16,6 +17,7 @@ import { computed } from 'vue';
 import { useRoute } from 'vue-router';
 import ModelsView from 'src/components/ModelsView.vue';
 import CreateModelDialog from 'components/dialog/CreateModelDialog.vue';
+import CreateAIModelDialog from 'components/dialog/CreateAIModelDialog.vue';
 import DeleteModelDialog from 'components/dialog/DeleteModelDialog.vue';
 import RenameModelDialog from 'components/dialog/RenameModelDialog.vue';
 import ImportModelTemplateDialog from 'components/dialog/ImportModelTemplateDialog.vue';
