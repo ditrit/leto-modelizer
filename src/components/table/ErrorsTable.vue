276f5f03778121404f463a6c0d658aafcc502121
Vincent Moittié <moittie.vincent@gmail.com>
2024-08-05 10:38:16 +0200

    Update ErrorsTable to make it adaptable for diagram or text editor page

--- src/components/table/ErrorsTable.vue
+++ src/components/table/ErrorsTable.vue
@@ -15,40 +15,74 @@ import { useI18n } from 'vue-i18n';
 
 const { t } = useI18n();
 
-defineProps({
+const props = defineProps({
   errors: {
     type: Array,
     required: true,
   },
+  editorType: {
+    type: String,
+    default: 'text',
+  },
 });
 
-const columns = computed(() => [
-  {
+const columns = computed(() => {
+  const severityColumn = {
     name: 'severity',
     align: 'left',
     label: t('footer.consoleFooter.errorsTable.severity'),
-    field: 'severity',
+    field: (row) => t(`parser.severity.${row.severity}`),
     style: 'width: 2rem',
-  },
-  {
-    name: 'line',
-    align: 'center',
-    label: t('footer.consoleFooter.errorsTable.line'),
-    field: (row) => `${row.startLine}-${row.endLine}`,
-    style: 'width: 2rem',
-  },
-  {
-    name: 'column',
-    align: 'center',
-    label: t('footer.consoleFooter.errorsTable.column'),
-    field: (row) => `${row.startColumn}-${row.endColumn}`,
-    style: 'width: 2rem',
-  },
-  {
+  };
+  const messageColumn = {
     name: 'message',
     align: 'left',
     label: t('footer.consoleFooter.errorsTable.message'),
-    field: 'message',
-  },
-]);
+    field: (row) => t(row.message, {
+      initialErrorMessage: row.initialErrorMessage,
+      extraData: row.extraData,
+      attribute: row.attribute,
+    }),
+  };
+
+  if (props.editorType === 'text') {
+    return [
+      severityColumn,
+      {
+        name: 'line',
+        align: 'center',
+        label: t('footer.consoleFooter.errorsTable.line'),
+        field: (row) => `${row.startLineNumber}-${row.endLineNumber}`,
+        style: 'width: 2rem',
+      },
+      {
+        name: 'column',
+        align: 'center',
+        label: t('footer.consoleFooter.errorsTable.column'),
+        field: (row) => `${row.startColumn}-${row.endColumn}`,
+        style: 'width: 2rem',
+      },
+      messageColumn,
+    ];
+  }
+
+  return [
+    severityColumn,
+    {
+      name: 'component',
+      align: 'center',
+      label: t('footer.consoleFooter.errorsTable.component'),
+      field: (row) => row.componentId,
+      style: 'width: 2rem',
+    },
+    {
+      name: 'attribute',
+      align: 'center',
+      label: t('footer.consoleFooter.errorsTable.attribute'),
+      field: (row) => row.attribute,
+      style: 'width: 2rem',
+    },
+    messageColumn,
+  ];
+});
 </script>
