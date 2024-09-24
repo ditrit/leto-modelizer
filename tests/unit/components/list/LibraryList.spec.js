4807d8c8fa8c994ff3258164a3180127946894d9
Vincent Moittié <moittie.vincent@gmail.com>
2024-09-24 16:50:30 +0200

    Update list of library to load templates

--- tests/unit/components/list/LibraryList.spec.js
+++ tests/unit/components/list/LibraryList.spec.js
@@ -18,6 +18,19 @@ jest.mock('vue-simple-acl', () => ({
   }),
 }));
 
+jest.mock('src/services/TemplateService', () => ({
+  getTemplatesByType: jest.fn(() => Promise.resolve({
+    content: [{
+      name: 'test1',
+    }],
+    totalElements: 1,
+    totalPages: 1,
+    pageable: {
+      pageNumber: 0,
+    },
+  })),
+}));
+
 describe('Test component: LibraryList', () => {
   let wrapper;
 
@@ -39,105 +52,95 @@ describe('Test component: LibraryList', () => {
             },
           },
         },
-        templates: [{
-          type: 'templateType',
-        }],
       },
     });
   });
 
-  describe('Test variables initialization', () => {
-    describe('Test computed: pluginDefinitions', () => {
-      it('should be a list of Object with "type" as key and "componentType" as value', () => {
-        expect(wrapper.vm.pluginDefinitions).toEqual([{
-          type: 'componentType',
-        }]);
-      });
+  describe('Test computed: pluginDefinitions', () => {
+    it('should be a list of Object with "type" as key and "componentType" as value', () => {
+      expect(wrapper.vm.pluginDefinitions).toEqual([{
+        type: 'componentType',
+      }]);
     });
+  });
 
-    describe('Test computed: templateDefinitions', () => {
-      it('should be a list of Object with type as key and "templateType" as value', () => {
-        expect(wrapper.vm.templateDefinitions).toEqual([{
-          type: 'templateType',
-        }]);
-      });
-    });
-
-    describe('Test computed: isEmptyList', () => {
-      it('should be true when pluginDefinitions and templateDefinitions are empty', async () => {
-        await wrapper.setProps({
-          plugin: {
-            data: {
-              name: 'pluginName',
-              definitions: {
-                components: [],
-              },
+  describe('Test computed: isEmptyList', () => {
+    it('should be true when pluginDefinitions and templateDefinitions are empty', async () => {
+      wrapper.vm.templates = [];
+      await wrapper.setProps({
+        plugin: {
+          data: {
+            name: 'pluginName',
+            definitions: {
+              components: [],
             },
           },
-          templates: [],
-        });
-
-        expect(wrapper.vm.isEmptyList).toBeTruthy();
+        },
       });
 
-      it('should be false when pluginDefinitions is not empty', async () => {
-        await wrapper.setProps({
-          templates: [],
-        });
+      expect(wrapper.vm.isEmptyList).toBeTruthy();
+    });
 
-        expect(wrapper.vm.isEmptyList).toBeFalsy();
-      });
+    it('should be false when pluginDefinitions is not empty', async () => {
+      wrapper.vm.templates = [];
 
-      it('should be false when templateDefinitions is not empty', async () => {
-        await wrapper.setProps({
-          plugin: {
-            data: {
-              name: 'pluginName',
-              definitions: {
-                components: [],
-              },
+      expect(wrapper.vm.pluginDefinitions).not.toEqual([]);
+      expect(wrapper.vm.isEmptyList).toBeFalsy();
+    });
+
+    it('should be false when templates is not empty', async () => {
+      await wrapper.setProps({
+        plugin: {
+          data: {
+            name: 'pluginName',
+            definitions: {
+              components: [],
             },
           },
-        });
-
-        expect(wrapper.vm.isEmptyList).toBeFalsy();
+        },
       });
+
+      expect(wrapper.vm.templates).not.toEqual([]);
+      expect(wrapper.vm.isEmptyList).toBeFalsy();
     });
+  });
 
-    describe('Test computed: drawerItems', () => {
-      it('should be a list of Object with "id", "title", "definitions", "size" as keys and corresponding data as values', () => {
-        expect(wrapper.vm.drawerItems).toEqual([
-          {
-            id: 'pluginName',
-            title: 'pluginName.displayName',
-            definitions: [{ type: 'componentType' }],
-            size: 1,
-            hasRole: true,
-          },
-          {
-            id: 'page.modelizer.drawer.templates.title',
-            title: 'page.modelizer.drawer.templates.title',
-            definitions: [{ type: 'templateType' }],
-            size: 1,
-            hasRole: false,
-          },
-        ]);
-      });
+  describe('Test computed: drawerItems', () => {
+    it('should be a list of Object with "id", "title", "definitions", "size" as keys and corresponding data as values', () => {
+      expect(wrapper.vm.drawerItems).toEqual([
+        {
+          id: 'pluginName',
+          title: 'pluginName.displayName',
+          definitions: [{ type: 'componentType' }],
+          size: 1,
+          hasRole: true,
+        },
+        {
+          id: 'page.modelizer.drawer.templates.title',
+          title: 'page.modelizer.drawer.templates.title',
+          definitions: [{
+            isTemplate: true,
+            name: 'test1',
+          }],
+          size: 1,
+          hasRole: false,
+        },
+      ]);
     });
+  });
 
-    describe('Test computed: hasRole', () => {
-      it('should be false without selected item', () => {
-        expect(wrapper.vm.selectedItemId).toBeNull();
-        expect(wrapper.vm.hasRole).toEqual(false);
-      });
-      it('should be false with selected item without role', () => {
-        wrapper.vm.selectedItemId = 'page.modelizer.drawer.templates.title';
-        expect(wrapper.vm.hasRole).toEqual(false);
-      });
-      it('should be true with selected item and role', () => {
-        wrapper.vm.selectedItemId = 'pluginName';
-        expect(wrapper.vm.hasRole).toEqual(true);
-      });
+  describe('Test computed: hasRole', () => {
+    it('should be false without selected item', () => {
+      expect(wrapper.vm.selectedItemId).toBeNull();
+      expect(wrapper.vm.hasRole).toEqual(false);
+    });
+    it('should be false with selected item without role', () => {
+      wrapper.vm.selectedItemId = 'page.modelizer.drawer.templates.title';
+      expect(wrapper.vm.hasRole).toEqual(false);
+    });
+    it('should be true with selected item and role', () => {
+      wrapper.vm.selectedItemId = 'pluginName';
+      expect(wrapper.vm.hasRole).toEqual(true);
     });
   });
 
@@ -158,6 +161,56 @@ describe('Test component: LibraryList', () => {
 
       expect(wrapper.vm.isSelected).toBeFalsy();
       expect(wrapper.vm.selectedItemId).toEqual(null);
+    });
+  });
+
+  describe('Test function: loadTemplates', () => {
+    it('should return templates', async () => {
+      wrapper.vm.loading = true;
+
+      const templates = await wrapper.vm.loadTemplates(0);
+
+      expect(wrapper.vm.loading).toEqual(false);
+      expect(templates).toEqual([{
+        isTemplate: true,
+        name: 'test1',
+      }]);
+    });
+  });
+
+  describe('Test function: initTemplates', () => {
+    it('should set templates', async () => {
+      wrapper.vm.templates = [];
+
+      await wrapper.vm.initTemplates();
+
+      expect(wrapper.vm.templates).toEqual([{
+        isTemplate: true,
+        name: 'test1',
+      }]);
+    });
+  });
+
+  describe('Test function: loadMoreTemplates', () => {
+    it('should add more templates', async () => {
+      wrapper.vm.templates = [];
+
+      await wrapper.vm.initTemplates();
+
+      expect(wrapper.vm.templates).toEqual([{
+        isTemplate: true,
+        name: 'test1',
+      }]);
+
+      await wrapper.vm.loadMoreTemplates();
+
+      expect(wrapper.vm.templates).toEqual([{
+        isTemplate: true,
+        name: 'test1',
+      }, {
+        isTemplate: true,
+        name: 'test1',
+      }]);
     });
   });
 });
