34be5d38c3826c9b91b56ca18acdcb87aab9e4ce
Vincent Moittié <moittie.vincent@gmail.com>
2024-09-25 14:22:18 +0200

    Update Template grid to load templates

--- tests/unit/components/grid/TemplateGrid.spec.js
+++ tests/unit/components/grid/TemplateGrid.spec.js
@@ -3,11 +3,31 @@ import { shallowMount } from '@vue/test-utils';
 import TemplateGrid from 'src/components/grid/TemplateGrid.vue';
 import { createI18n } from 'vue-i18n';
 import i18nConfiguration from 'src/i18n';
+import { getTemplatesByType } from 'src/services/TemplateService';
 
 installQuasarPlugin();
 
 jest.mock('src/composables/Filter', () => ({
   searchText: jest.fn(() => true),
+}));
+
+jest.mock('vue-simple-acl', () => ({
+  useAcl: () => ({
+    role(key) {
+      return key === 'create-project-from-template';
+    },
+  }),
+}));
+
+jest.mock('src/services/TemplateService', () => ({
+  getTemplatesByType: jest.fn(() => Promise.resolve({
+    content: [{ id: 'id_1' }],
+    pageable: {
+      pageNumber: 0,
+      pageSize: 5,
+    },
+    totalElements: 1,
+  })),
 }));
 
 describe('Test component: TemplateGrid', () => {
@@ -16,7 +36,7 @@ describe('Test component: TemplateGrid', () => {
   beforeEach(() => {
     wrapper = shallowMount(TemplateGrid, {
       props: {
-        templates: [{ type: 'Template1' }],
+        type: 'PROJECT',
       },
       global: {
         plugins: [
@@ -29,17 +49,37 @@ describe('Test component: TemplateGrid', () => {
     });
   });
 
-  describe('Test props initialization', () => {
-    describe('Test prop: templates', () => {
-      it('should match array of templates', () => {
-        expect(wrapper.vm.templates).toStrictEqual([{ type: 'Template1' }]);
-      });
-    });
-  });
+  describe('Test function : loadTemplates', () => {
+    it('should not load templates without valid role', async () => {
+      getTemplatesByType.mockClear();
+      await wrapper.setProps({ type: 'DIAGRAM' });
+      await wrapper.vm.loadTemplates({ pagination: null });
 
-  describe('Test computed: filteredTemplates', () => {
-    it('should match props.templates', () => {
-      expect(wrapper.vm.filteredTemplates).toEqual([{ type: 'Template1' }]);
+      expect(getTemplatesByType).not.toBeCalled();
+      expect(wrapper.vm.templates).toEqual([]);
+    });
+
+    it('should not load templates without valid role', async () => {
+      getTemplatesByType.mockClear();
+      wrapper.vm.loading = true;
+      await wrapper.setProps({ type: 'PROJECT' });
+      await wrapper.vm.loadTemplates({
+        pagination: {
+          page: 0,
+          rowsPerPage: 5,
+        },
+      });
+
+      expect(getTemplatesByType).toBeCalled();
+      expect(wrapper.vm.templates).toEqual([{
+        id: 'id_1',
+      }]);
+      expect(wrapper.vm.pagination).toEqual({
+        page: 1,
+        rowsNumber: 1,
+        rowsPerPage: 5,
+      });
+      expect(wrapper.vm.loading).toEqual(false);
     });
   });
 });
